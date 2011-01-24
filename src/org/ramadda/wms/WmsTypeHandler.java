2e1970eef8c3a8dc03b321227cc06d9ba079b41b
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-01-24 18:14:27 +0000

    my bad

---
+++ src/org/ramadda/wms/WmsTypeHandler.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2008-2011 Jeff McWhirter/ramadda.org
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+
+package org.ramadda.wms;
+
+
+import org.w3c.dom.*;
+
+import ucar.unidata.repository.*;
+import ucar.unidata.repository.output.*;
+import ucar.unidata.repository.type.*;
+
+
+/**
+ * A place holder class that provides services for WMS URL entry types.
+ * Right now this does nothing but we could use it to provide a new defalt html display
+ */
+public class WmsTypeHandler extends GenericTypeHandler {
+
+
+    /**
+     * ctor
+     */
+    public WmsTypeHandler() {}
+
+
+    /**
+     * ctor
+     *
+     * @param repository the repository
+     * @param node the types.xml node
+     * @throws Exception On badness
+     */
+    public WmsTypeHandler(Repository repository, Element node)
+            throws Exception {
+        super(repository, node);
+    }
+
+}
