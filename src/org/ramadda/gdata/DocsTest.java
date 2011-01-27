083564f2eb1310629bf1bcd0b3dbea5b3e338f40
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-01-27 23:41:22 +0000

    new classes

---
+++ src/org/ramadda/gdata/DocsTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2008-2011 Jeff McWhirter/ramadda.org
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+
+import com.google.gdata.client.*;
+import com.google.gdata.client.*;
+import com.google.gdata.client.calendar.*;
+
+import com.google.gdata.client.docs.*;
+import com.google.gdata.client.photos.*;
+import com.google.gdata.data.MediaContent;
+import com.google.gdata.data.acl.*;
+import com.google.gdata.data.docs.*;
+import com.google.gdata.data.extensions.*;
+import com.google.gdata.data.extensions.*;
+import com.google.gdata.data.media.*;
+import com.google.gdata.data.photos.*;
+import com.google.gdata.util.*;
+import com.google.gdata.util.*;
+
+import ucar.unidata.util.IOUtil;
+
+import java.net.URL;
+
+import java.util.Hashtable;
+import java.util.List;
+
+
+/**
+ * Class description
+ *
+ *
+ * @version        $version$, Wed, Jan 26, '11
+ * @author         Enter your name here...    
+ */
+public class DocsTest {
+
+
+    /**
+     * _more_
+     *
+     * @param args _more_
+     *
+     * @throws Exception _more_
+     */
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: DocsTest <userid> <password>");
+            return;
+        }
+        DocsService client = new DocsService("ramadda");
+        client.setUserCredentials(args[0], args[1]);
+
+        String[] urls = {
+            //            "https://docs.google.com/feeds/default/private/full/folder%3Aroot/contents?showfolders=true",
+            "https://docs.google.com/feeds/default/private/full?showfolders=true"
+        };
+        for (String url : urls) {
+            DocumentQuery    query      = new DocumentQuery(new URL(url));
+            DocumentListFeed allEntries = new DocumentListFeed();
+            DocumentListFeed tempFeed = client.getFeed(query,
+                                            DocumentListFeed.class);
+            do {
+                allEntries.getEntries().addAll(tempFeed.getEntries());
+                com.google.gdata.data.Link link = tempFeed.getNextLink();
+                if (link == null) {
+                    break;
+                }
+                tempFeed = client.getFeed(new URL(link.getHref()),
+                                          DocumentListFeed.class);
+            } while (tempFeed.getEntries().size() > 0);
+
+            System.out.println("query url:" + url);
+            System.out.println("Found " + allEntries.getEntries().size()
+                               + " entries");
+            Hashtable<String, DocumentListEntry> map =
+                new Hashtable<String, DocumentListEntry>();
+            for (DocumentListEntry entry : allEntries.getEntries()) {
+                String id = IOUtil.getFileTail(entry.getId());
+                map.put(id, entry);
+            }
+            for (DocumentListEntry entry : allEntries.getEntries()) {
+                String id = IOUtil.getFileTail(entry.getId());
+                java.util.List<com.google.gdata.data.Link> links =
+                    entry.getParentLinks();
+                if (links.size() == 0) {
+                    System.out.println("Top level:"
+                                       + entry.getTitle().getPlainText());
+                } else {
+                    String parentId =
+                        IOUtil.getFileTail(links.get(0).getHref());
+                    DocumentListEntry parent = map.get(parentId);
+                    System.out.println(entry.getTitle().getPlainText()
+                                       + " parent:"
+                                       + parent.getTitle().getPlainText());
+                }
+            }
+        }
+    }
+
+
+}
+
---
+++ src/org/ramadda/gdata/GdataBaseTypeHandler.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2008-2011 Jeff McWhirter/ramadda.org
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+
+package org.ramadda.gdata;
+
+
+import com.google.gdata.client.GoogleService;
+
+import com.google.gdata.data.BaseEntry;
+import com.google.gdata.data.Category;
+
+import com.google.gdata.data.Person;
+
+
+import org.w3c.dom.*;
+
+
+import ucar.unidata.repository.*;
+import ucar.unidata.repository.metadata.Metadata;
+import ucar.unidata.repository.type.*;
+
+import java.io.File;
+
+import java.net.URL;
+
+import java.util.Hashtable;
+import java.util.List;
+
+
+
+
+
+
+import java.util.Set;
+
+
+/**
+ * Class TypeHandler _more_
+ *
+ *
+ * @author IDV Development Team
+ * @version $Revision: 1.3 $
+ */
+public class GdataBaseTypeHandler extends GenericTypeHandler {
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public GdataBaseTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param id _more_
+     *
+     * @return _more_
+     */
+    public Entry createEntry(String id) {
+        //Make the top level entyr act like a group
+        return new Entry(id, this, true);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param newEntry _more_
+     * @param baseEntry _more_
+     *
+     * @throws Exception _more_
+     */
+    public void addMetadata(Entry newEntry, BaseEntry baseEntry)
+            throws Exception {
+        addMetadata(newEntry, baseEntry, null);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param newEntry _more_
+     * @param baseEntry _more_
+     * @param desc _more_
+     *
+     * @throws Exception _more_
+     */
+    public void addMetadata(Entry newEntry, BaseEntry baseEntry,
+                            StringBuffer desc)
+            throws Exception {
+        if ((baseEntry.getSummary() != null) && (desc != null)) {
+            desc.append(baseEntry.getSummary().getPlainText());
+        }
+
+
+
+        for (Category category : (Set<Category>) baseEntry.getCategories()) {
+            if (category.getLabel() == null) {
+                continue;
+            }
+            newEntry.addMetadata(new Metadata(getRepository().getGUID(),
+                    newEntry.getId(), "enum_tag", false, category.getLabel(),
+                    "", "", "", ""));
+        }
+
+        for (Person person : (List<Person>) baseEntry.getAuthors()) {
+            newEntry.addMetadata(new Metadata(getRepository().getGUID(),
+                    newEntry.getId(), "gdata.author", false,
+                    person.getName(), person.getEmail(), "", "", ""));
+        }
+        for (Person person : (List<Person>) baseEntry.getContributors()) {
+            newEntry.addMetadata(new Metadata(getRepository().getGUID(),
+                    newEntry.getId(), "gdata.contributor", false,
+                    person.getName(), person.getEmail(), "", "", ""));
+        }
+
+        if (baseEntry.getRights() != null) {
+            String rights = baseEntry.getRights().getPlainText();
+            if ((rights != null) && (rights.length() > 0)) {
+                newEntry.addMetadata(new Metadata(getRepository().getGUID(),
+                        newEntry.getId(), "gdata.rights", false, rights, "",
+                        "", "", ""));
+
+            }
+        }
+    }
+
+
+
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
+    public boolean isSynthType() {
+        return true;
+    }
+
+
+
+
+
+
+}
+
