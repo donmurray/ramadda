6461e604de4478b4d3b84e95a2cdc2feb44eb060
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-01-30 02:53:39 +0000

    new package

---
+++ src/org/ramadda/plugins/forum/ForumTypeHandler.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2008-2011 Jeff McWhirter/ramadda.org
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+
+package org.ramadda.plugins.forum;
+
+
+import org.w3c.dom.*;
+
+import ucar.unidata.repository.*;
+import ucar.unidata.repository.auth.*;
+import ucar.unidata.repository.metadata.*;
+import ucar.unidata.repository.output.*;
+import ucar.unidata.repository.type.*;
+
+
+import ucar.unidata.sql.Clause;
+
+
+import ucar.unidata.sql.SqlUtil;
+import ucar.unidata.sql.SqlUtil;
+import ucar.unidata.util.DateUtil;
+
+import ucar.unidata.util.HtmlUtil;
+import ucar.unidata.util.HttpServer;
+import ucar.unidata.util.IOUtil;
+import ucar.unidata.util.LogUtil;
+import ucar.unidata.util.Misc;
+import ucar.unidata.util.StringUtil;
+import ucar.unidata.util.WikiUtil;
+import ucar.unidata.xml.XmlUtil;
+
+import java.sql.PreparedStatement;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
+
+/**
+ *
+ *
+ */
+public class ForumTypeHandler extends ExtensibleGroupTypeHandler {
+
+
+    /** _more_ */
+    public static String TYPE_FORUM = "forum";
+
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public ForumTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param group _more_
+     * @param subGroups _more_
+     * @param entries _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    public Result getHtmlDisplay(Request request, Entry group,
+                                 List<Entry> subGroups, List<Entry> entries)
+            throws Exception {
+        return null;
+    }
+
+
+
+
+}
