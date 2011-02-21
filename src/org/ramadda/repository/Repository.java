222d6c54a6afd7a2a683fe5bf94058e91a013e4e
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-02-21 13:11:01 +0000

    cleanup

--- src/org/ramadda/repository/Repository.java
+++ src/org/ramadda/repository/Repository.java
@@ -988,12 +988,10 @@ public class Repository extends RepositoryBase implements RequestHandler, Proper
         }
 
         String derbyHome = (String) properties.get(PROP_DB_DERBY_HOME);
-        System.err.println("RAMADDA: derbyHome from properties:" + derbyHome);
         if (derbyHome != null) {
             derbyHome = getStorageManager().localizePath(derbyHome);
             File dir = new File(derbyHome);
             IOUtil.makeDirRecursive(dir);
-            System.err.println("RAMADDA: derbyHome =" + derbyHome);
             System.setProperty("derby.system.home", derbyHome);
         }
 
