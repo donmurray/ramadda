4807d3b1c27a20c6dfc3f0698c3d6bf6520ab84a
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-02-25 20:20:17 +0000

    snapshot

---
+++ src/org/ramadda/geodata/echo/EchoUtil.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2008-2011 Jeff McWhirter/ramadda.org
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+
+package org.ramadda.geodata.echo;
+
+
+
+
+/**
+ *
+ */
+
+public class EchoUtil {
+
+public static final String XMLNS_XMLNS_XSI = "http://www.w3.org/2001/XMLSchema-instance";
+
+public static final String TAG_COLLECTIONMETADATAFILE = "CollectionMetaDataFile";
+public static final String TAG_COLLECTIONS = "Collections";
+public static final String TAG_COLLECTION = "Collection";
+public static final String TAG_SHORTNAME = "ShortName";
+public static final String TAG_VERSIONID = "VersionId";
+public static final String TAG_INSERTTIME = "InsertTime";
+public static final String TAG_LASTUPDATE = "LastUpdate";
+public static final String TAG_LONGNAME = "LongName";
+public static final String TAG_DATASETID = "DataSetId";
+public static final String TAG_DESCRIPTION = "Description";
+public static final String TAG_ORDERABLE = "Orderable";
+public static final String TAG_VISIBLE = "Visible";
+public static final String TAG_PROCESSINGLEVELID = "ProcessingLevelId";
+public static final String TAG_ARCHIVECENTER = "ArchiveCenter";
+public static final String TAG_RESTRICTIONFLAG = "RestrictionFlag";
+public static final String TAG_PRICE = "Price";
+public static final String TAG_SPATIALKEYWORDS = "SpatialKeywords";
+public static final String TAG_KEYWORD = "Keyword";
+public static final String TAG_TEMPORALKEYWORDS = "TemporalKeywords";
+public static final String TAG_TEMPORAL = "Temporal";
+public static final String TAG_RANGEDATETIME = "RangeDateTime";
+public static final String TAG_BEGINNINGDATETIME = "BeginningDateTime";
+public static final String TAG_ENDINGDATETIME = "EndingDateTime";
+public static final String TAG_CONTACTS = "Contacts";
+public static final String TAG_CONTACT = "Contact";
+public static final String TAG_ROLE = "Role";
+public static final String TAG_HOURSOFSERVICE = "HoursOfService";
+public static final String TAG_ORGANIZATIONNAME = "OrganizationName";
+public static final String TAG_ORGANIZATIONADDRESSES = "OrganizationAddresses";
+public static final String TAG_ADDRESS = "Address";
+public static final String TAG_STREETADDRESS = "StreetAddress";
+public static final String TAG_CITY = "City";
+public static final String TAG_STATEPROVINCE = "StateProvince";
+public static final String TAG_POSTALCODE = "PostalCode";
+public static final String TAG_COUNTRY = "Country";
+public static final String TAG_ORGANIZATIONPHONES = "OrganizationPhones";
+public static final String TAG_PHONE = "Phone";
+public static final String TAG_NUMBER = "Number";
+public static final String TAG_TYPE = "Type";
+public static final String TAG_ORGANIZATIONEMAILS = "OrganizationEmails";
+public static final String TAG_EMAIL = "Email";
+public static final String TAG_CONTACTPERSONS = "ContactPersons";
+public static final String TAG_CONTACTPERSON = "ContactPerson";
+public static final String TAG_FIRSTNAME = "FirstName";
+public static final String TAG_MIDDLENAME = "MiddleName";
+public static final String TAG_LASTNAME = "LastName";
+public static final String TAG_SCIENCEKEYWORDS = "ScienceKeywords";
+public static final String TAG_SCIENCEKEYWORD = "ScienceKeyword";
+public static final String TAG_CATEGORYKEYWORD = "CategoryKeyword";
+public static final String TAG_TOPICKEYWORD = "TopicKeyword";
+public static final String TAG_TERMKEYWORD = "TermKeyword";
+public static final String TAG_VARIABLELEVEL1KEYWORD = "VariableLevel1Keyword";
+public static final String TAG_VALUE = "Value";
+public static final String TAG_VARIABLELEVEL2KEYWORD = "VariableLevel2Keyword";
+public static final String TAG_VARIABLELEVEL3KEYWORD = "VariableLevel3Keyword";
+public static final String TAG_DETAILEDVARIABLEKEYWORD = "DetailedVariableKeyword";
+public static final String TAG_PLATFORMS = "Platforms";
+public static final String TAG_PLATFORM = "Platform";
+public static final String TAG_CHARACTERISTICS = "Characteristics";
+public static final String TAG_CHARACTERISTIC = "Characteristic";
+public static final String TAG_NAME = "Name";
+public static final String TAG_DATATYPE = "DataType";
+public static final String TAG_UNIT = "Unit";
+public static final String TAG_INSTRUMENTS = "Instruments";
+public static final String TAG_INSTRUMENT = "Instrument";
+public static final String TAG_SENSORS = "Sensors";
+public static final String TAG_SENSOR = "Sensor";
+public static final String TAG_TECHNIQUE = "Technique";
+public static final String TAG_ADDITIONALATTRIBUTES = "AdditionalAttributes";
+public static final String TAG_ADDITIONALATTRIBUTE = "AdditionalAttribute";
+public static final String TAG_CAMPAIGNS = "Campaigns";
+public static final String TAG_CAMPAIGN = "Campaign";
+public static final String TAG_TWODCOORDINATESYSTEMS = "TwoDCoordinateSystems";
+public static final String TAG_TWODCOORDINATESYSTEM = "TwoDCoordinateSystem";
+public static final String TAG_TWODCOORDINATESYSTEMNAME = "TwoDCoordinateSystemName";
+public static final String TAG_COORDINATE1 = "Coordinate1";
+public static final String TAG_MINIMUMVALUE = "MinimumValue";
+public static final String TAG_MAXIMUMVALUE = "MaximumValue";
+public static final String TAG_COORDINATE2 = "Coordinate2";
+public static final String TAG_ONLINEACCESSURLS = "OnlineAccessURLs";
+public static final String TAG_ONLINEACCESSURL = "OnlineAccessURL";
+public static final String TAG_URL = "URL";
+public static final String TAG_URLDESCRIPTION = "URLDescription";
+public static final String TAG_MIMETYPE = "MimeType";
+public static final String TAG_ONLINERESOURCES = "OnlineResources";
+public static final String TAG_ONLINERESOURCE = "OnlineResource";
+public static final String TAG_ASSOCIATEDDIFS = "AssociatedDIFs";
+public static final String TAG_DIF = "DIF";
+public static final String TAG_ENTRYID = "EntryId";
+public static final String TAG_SPATIAL = "Spatial";
+public static final String TAG_SPATIALCOVERAGETYPE = "SpatialCoverageType";
+public static final String TAG_HORIZONTALSPATIALDOMAIN = "HorizontalSpatialDomain";
+public static final String TAG_ZONEIDENTIFIER = "ZoneIdentifier";
+public static final String TAG_GEOMETRY = "Geometry";
+public static final String TAG_COORDINATESYSTEM = "CoordinateSystem";
+public static final String TAG_BOUNDINGRECTANGLE = "BoundingRectangle";
+public static final String TAG_WESTBOUNDINGCOORDINATE = "WestBoundingCoordinate";
+public static final String TAG_NORTHBOUNDINGCOORDINATE = "NorthBoundingCoordinate";
+public static final String TAG_EASTBOUNDINGCOORDINATE = "EastBoundingCoordinate";
+public static final String TAG_SOUTHBOUNDINGCOORDINATE = "SouthBoundingCoordinate";
+public static final String TAG_VERTICALSPATIALDOMAIN = "VerticalSpatialDomain";
+public static final String TAG_GRANULESPATIALREPRESENTATION = "GranuleSpatialRepresentation";
+public static final String TAG_ASSOCIATEDBROWSEIMAGES = "AssociatedBrowseImages";
+public static final String TAG_PROVIDERBROWSEID = "ProviderBrowseId";
+public static final String TAG_COLLECTIONDELETES = "CollectionDeletes";
+public static final String TAG_COLLECTIONDELETE = "CollectionDelete";
+public static final String TAG_GRANULEMETADATAFILE = "GranuleMetaDataFile";
+public static final String TAG_GRANULES = "Granules";
+public static final String TAG_GRANULE = "Granule";
+public static final String TAG_GRANULEUR = "GranuleUR";
+public static final String TAG_RESTRICTIONCOMMENT = "RestrictionComment";
+public static final String TAG_DATAGRANULE = "DataGranule";
+public static final String TAG_SIZEMBDATAGRANULE = "SizeMBDataGranule";
+public static final String TAG_DAYNIGHTFLAG = "DayNightFlag";
+public static final String TAG_PRODUCTIONDATETIME = "ProductionDateTime";
+public static final String TAG_LOCALVERSIONID = "LocalVersionId";
+public static final String TAG_PGEVERSIONCLASS = "PGEVersionClass";
+public static final String TAG_PGENAME = "PGEName";
+public static final String TAG_PGEVERSION = "PGEVersion";
+public static final String TAG_SINGLEDATETIME = "SingleDateTime";
+public static final String TAG_VERTICALSPATIALDOMAINS = "VerticalSpatialDomains";
+public static final String TAG_ORBITCALCULATEDSPATIALDOMAINS = "OrbitCalculatedSpatialDomains";
+public static final String TAG_ORBITCALCULATEDSPATIALDOMAIN = "OrbitCalculatedSpatialDomain";
+public static final String TAG_ORBITALMODELNAME = "OrbitalModelName";
+public static final String TAG_ORBITNUMBER = "OrbitNumber";
+public static final String TAG_STARTORBITNUMBER = "StartOrbitNumber";
+public static final String TAG_STOPORBITNUMBER = "StopOrbitNumber";
+public static final String TAG_EQUATORCROSSINGLONGITUDE = "EquatorCrossingLongitude";
+public static final String TAG_EQUATORCROSSINGDATETIME = "EquatorCrossingDateTime";
+public static final String TAG_MEASUREDPARAMETERS = "MeasuredParameters";
+public static final String TAG_MEASUREDPARAMETER = "MeasuredParameter";
+public static final String TAG_PARAMETERNAME = "ParameterName";
+public static final String TAG_QASTATS = "QAStats";
+public static final String TAG_QAPERCENTMISSINGDATA = "QAPercentMissingData";
+public static final String TAG_QAPERCENTOUTOFBOUNDSDATA = "QAPercentOutOfBoundsData";
+public static final String TAG_QAPERCENTINTERPOLATEDDATA = "QAPercentInterpolatedData";
+public static final String TAG_QAPERCENTCLOUDCOVER = "QAPercentCloudCover";
+public static final String TAG_QAFLAGS = "QAFlags";
+public static final String TAG_AUTOMATICQUALITYFLAG = "AutomaticQualityFlag";
+public static final String TAG_AUTOMATICQUALITYFLAGEXPLANATION = "AutomaticQualityFlagExplanation";
+public static final String TAG_OPERATIONALQUALITYFLAG = "OperationalQualityFlag";
+public static final String TAG_OPERATIONALQUALITYFLAGEXPLANATION = "OperationalQualityFlagExplanation";
+public static final String TAG_SCIENCEQUALITYFLAG = "ScienceQualityFlag";
+public static final String TAG_SCIENCEQUALITYFLAGEXPLANATION = "ScienceQualityFlagExplanation";
+public static final String TAG_VALUES = "Values";
+public static final String TAG_STARTCOORDINATE1 = "StartCoordinate1";
+public static final String TAG_ENDCOORDINATE1 = "EndCoordinate1";
+public static final String TAG_STARTCOORDINATE2 = "StartCoordinate2";
+public static final String TAG_ENDCOORDINATE2 = "EndCoordinate2";
+public static final String TAG_DATAFORMAT = "DataFormat";
+public static final String TAG_CLOUDCOVER = "CloudCover";
+public static final String TAG_GRANULEDELETES = "GranuleDeletes";
+public static final String TAG_GRANULEDELETE = "GranuleDelete";
+public static final String TAG_BROWSEMETADATAFILE = "BrowseMetaDataFile";
+public static final String TAG_BROWSEIMAGES = "BrowseImages";
+public static final String TAG_BROWSEIMAGE = "BrowseImage";
+public static final String TAG_FILEURL = "FileURL";
+public static final String TAG_FILESIZE = "FileSize";
+public static final String TAG_COLLECTIONPARTIALADDS = "CollectionPartialAdds";
+public static final String TAG_COLLECTIONPARTIALADD = "CollectionPartialAdd";
+public static final String TAG_TARGETS = "Targets";
+public static final String TAG_TARGET = "Target";
+public static final String TAG_FIELDS = "Fields";
+public static final String TAG_FIELD = "Field";
+public static final String TAG_COLLECTIONPARTIALDELETES = "CollectionPartialDeletes";
+public static final String TAG_COLLECTIONPARTIALDELETE = "CollectionPartialDelete";
+public static final String TAG_GRANULEPARTIALADDS = "GranulePartialAdds";
+public static final String TAG_GRANULEPARTIALADD = "GranulePartialAdd";
+public static final String TAG_GRANULEPARTIALDELETES = "GranulePartialDeletes";
+public static final String TAG_GRANULEPARTIALDELETE = "GranulePartialDelete";
+public static final String TAG_BROWSEIMAGEDELETES = "BrowseImageDeletes";
+public static final String TAG_BROWSEIMAGEDELETE = "BrowseImageDelete";
+
+
+
+}
