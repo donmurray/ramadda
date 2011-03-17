8810d8df9f19da09cc767c517cb55e83dfa7bd4d
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-03-17 10:26:34 +0000

    document the permgen size setting

--- src/org/ramadda/repository/release/ramadda.bat
+++ src/org/ramadda/repository/release/ramadda.bat
@@ -1,16 +1,16 @@
 REM To run RAMADDA stand-alone just do
 
-java -Xmx512m -jar lib/@REPOSITORYJAR@ -port 8080 %*
+java -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 %*
 
 
 REM This will create a directory under ~/unidata/repository to store content and the database
 REM To change the directory do:
-REM java -Xmx512m -jar lib/@REPOSITORYJAR@ -port 8080 -Dramadda_home=/some/other/directory
+REM java -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 -Dramadda_home=/some/other/directory
 
 
 REM The default above is to use Java Derby as the database
 REM To run with mysql you do:
-REM java -Xmx512m -jar ramadda.jar -Dramadda.db=mysql
+REM java -Xmx512m -XX:MaxPermSize=256m -jar ramadda.jar -Dramadda.db=mysql
 
 REM Or see for more information: 
 REM http://www.unidata.ucar.edu/software/ramadda/docs/developer/ 
--- src/org/ramadda/repository/release/ramadda.sh
+++ src/org/ramadda/repository/release/ramadda.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-
 ## To run RAMADDA stand-alone just do
 echo 'Running RAMADDA on port 8080'
 echo 'Access it at: http://localhost:8080/repository'
-java -Xmx512m -jar lib/@REPOSITORYJAR@ -port 8080 $*
+java -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 $*
 
 
 
 ##This will create a directory under ~/unidata/repository to store content and the database
 #To change the directory do:
-##java -Xmx512m -jar lib/@REPOSITORYJAR@ -port 8080 -Dramadda_home=/some/other/directory
+##java -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 -Dramadda_home=/some/other/directory
 
 
 #The default above is to use Java Derby as the database
 #To run with mysql you do:
-#java -Xmx512m -jar ramadda.jar -Dramadda.db=mysql
+#java -Xmx512m -XX:MaxPermSize=256m -jar ramadda.jar -Dramadda.db=mysql
 
 ##Or see for more information: 
 ##http://www.unidata.ucar.edu/software/ramadda/docs/developer/ 
