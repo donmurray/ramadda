107a765ab2b6a31f4740aaaba3755ebafa4b2174
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-03-18 20:54:04 +0000

    snapshot

--- src/org/ramadda/plugins/beforeafter/BeforeAfterTypeHandler.java
+++ src/org/ramadda/plugins/beforeafter/BeforeAfterTypeHandler.java
@@ -44,6 +44,13 @@ import ucar.unidata.util.Misc;
 import ucar.unidata.util.StringUtil;
 import ucar.unidata.util.WikiUtil;
 import ucar.unidata.xml.XmlUtil;
+import ucar.unidata.ui.ImageUtils;
+
+
+import java.awt.Image;
+
+
+
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -52,6 +59,8 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Properties;
 
+import java.awt.Dimension;
+
 
 /**
  *
@@ -59,6 +68,7 @@ import java.util.Properties;
  */
 public class BeforeAfterTypeHandler extends GenericTypeHandler {
 
+    private Hashtable<String,Dimension> dimensions = new Hashtable<String,Dimension>();
 
 
     /**
@@ -78,6 +88,14 @@ public class BeforeAfterTypeHandler extends GenericTypeHandler {
         return true;
     }
 
+    public Result getHtmlDisplay(Request request, Entry entry)
+            throws Exception {
+        List<Entry> children =  getEntryManager().getChildren(request, entry);
+        return getHtmlDisplay(request, entry, new ArrayList<Entry>(), children);
+    }
+
+    private static 	int cnt = 0;
+
 
     /**
      * _more_
@@ -91,46 +109,79 @@ public class BeforeAfterTypeHandler extends GenericTypeHandler {
      *
      * @throws Exception _more_
      */
+
     public Result getHtmlDisplay(Request request, Entry group,
                                  List<Entry> subGroups, List<Entry> entries)
             throws Exception {
         StringBuffer sb = new StringBuffer();
 	StringBuffer divs = new StringBuffer();
 	int col =1;
-	int cnt = 0;
 	sb.append(HtmlUtil.importJS("/beforeandafter/jquery.beforeafter.js"));
         String template = getRepository().getResource("/org/ramadda/plugins/beforeafter/template.html");
 	//	sb.append(template);
 
+        List<Entry> entriesToUse = new ArrayList<Entry>();
 	for (Entry child : entries) {
 	    if ( !child.getResource().isImage()) {
 		continue;
 	    }
-	    String id = "container" + cnt;
-	    divs.append("\n");
-	    if(col==1) {
-		divs.append("<div id=\"" +  id +"\">\n");
-	    }
-	    String url = HtmlUtil.url(
+            entriesToUse.add(child);
+        }
+
+        for(int i=0;i<entriesToUse.size();i+=2) {
+            if(i>=entriesToUse.size()-1) break;
+            Entry entry1 = entriesToUse.get(i);
+            Entry entry2 = entriesToUse.get(i+1);
+            Dimension dim =  dimensions.get(entry1.getId());
+
+            if(dim == null) {
+               Image image =
+                   ImageUtils.readImage(entry1.getResource().getPath());
+               ImageUtils.waitOnImage(image);
+               dim = new Dimension(image.getWidth(null),image.getHeight(null));
+               if(dim.width>0 && dim.height>0) {
+                   dimensions.put(entry1.getId(), dim);
+               }
+               System.err.println(dim);
+            }
+
+            int width =  600;
+            int height =  366;
+
+            if(dim.width>0 && dim.height>0) {
+                width = dim.width;
+                height = dim.height;
+            }
+
+
+            if(entry1.getCreateDate()>entry2.getCreateDate()) {
+                Entry tmp = entry1;
+                entry1 = entry2;
+                entry2 = tmp;
+            }
+	    String id = "bandacontainer" + (cnt++);
+            divs.append("<div id=\"" +  id +"\">\n");
+	    String url1 = HtmlUtil.url(
 				      request.url(repository.URL_ENTRY_GET) + "/"
 				      + getStorageManager().getFileTail(
-									child), ARG_ENTRYID, child.getId());
+									entry1), ARG_ENTRYID, entry1.getId());
+	    String url2 = HtmlUtil.url(
+				      request.url(repository.URL_ENTRY_GET) + "/"
+				      + getStorageManager().getFileTail(
+									entry2), ARG_ENTRYID, entry2.getId());
 
-	    divs.append(HtmlUtil.div("<img src=\"" + url  +"\"" + 
-				     HtmlUtil.attr(HtmlUtil.ATTR_WIDTH,"600")+
-				     HtmlUtil.attr(HtmlUtil.ATTR_HEIGHT,"366") +">",""));
-	    divs.append("\n");
-	    if(col==2) {
-		divs.append("</div>\n");
-		String path = getRepository().getUrlBase() +"/beforeandafter/";
-		String args = "{imagePath:'" + path +"'}";
-		divs.append("\n");
-		sb.append("\n");
-		sb.append(HtmlUtil.script("\n$(function(){$('#container" + (cnt-1) +"').beforeAfter(" + args +");});\n"));
-		col=1;
-	    }
-	    col++;
-	    cnt++;
+	    divs.append(HtmlUtil.div("<img src=\"" + url1  +"\"" + 
+				     HtmlUtil.attr(HtmlUtil.ATTR_WIDTH,""+width)+
+				     HtmlUtil.attr(HtmlUtil.ATTR_HEIGHT,""+height) +">",""));
+
+	    divs.append(HtmlUtil.div("<img src=\"" + url2  +"\"" + 
+				     HtmlUtil.attr(HtmlUtil.ATTR_WIDTH,""+width)+
+				     HtmlUtil.attr(HtmlUtil.ATTR_HEIGHT,""+height) +">",""));
+            divs.append("</div>\n");
+            String path = getRepository().getUrlBase() +"/beforeandafter/";
+            String args = "{imagePath:'" + path +"'}";
+            sb.append("\n");
+            sb.append(HtmlUtil.script("\n$(function(){$('#" +  id +"').beforeAfter(" + args +");});\n"));
 	}
 	sb.append("\n");
 	sb.append(divs);
