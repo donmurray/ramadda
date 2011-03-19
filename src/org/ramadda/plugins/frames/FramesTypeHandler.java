63a22874792f07b3c490eca7273574df0c0df37f
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-03-19 11:52:16 +0000

    new plugin

---
+++ src/org/ramadda/plugins/frames/FramesTypeHandler.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2008-2011 Jeff McWhirter/ramadda.org
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+
+package org.ramadda.plugins.frames;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.type.*;
+
+import org.w3c.dom.*;
+
+import ucar.unidata.util.HtmlUtil;
+import ucar.unidata.util.StringUtil;
+
+import java.util.List;
+
+
+/**
+ *
+ *
+ */
+public class FramesTypeHandler extends GenericTypeHandler {
+
+
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public FramesTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param group _more_
+     * @param subGroups _more_
+     * @param entries _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    public Result getHtmlDisplay(Request request, Entry entry)
+            throws Exception {
+        StringBuffer sb = new StringBuffer();
+        String urls = entry.getValue(0,"");
+        String height = entry.getValue(1,"300");
+        int cols = Integer.parseInt(entry.getValue(2,"1"));
+        sb.append("<table width=100%><tr valign=top>");
+        int colCnt = 0;
+        for(String url: StringUtil.split(urls,"\n",true,true)) {
+            sb.append("<td>");
+            sb.append(HtmlUtil.href(url, url));
+            sb.append(HtmlUtil.tag(HtmlUtil.TAG_IFRAME,
+                                   HtmlUtil.attr(HtmlUtil.ATTR_SRC, url) + 
+                                   HtmlUtil.attr(HtmlUtil.ATTR_WIDTH,"100%") +
+                                   HtmlUtil.attr(HtmlUtil.ATTR_HEIGHT,height),"Need frames"));
+            sb.append("</td>");
+            colCnt++;
+            if(colCnt>=cols) {
+                sb.append("</tr><tr valign=top>");
+            }
+        }
+        sb.append("</tr></table>");
+        return new Result(msg("Frames"), sb);
+    }
+
+
+
+
+}
