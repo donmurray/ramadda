2b51cc3450f16a6f02c241d67d6f4f7208eedee7
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-07-29 03:41:36 +0000

    Roll in Matias' changes

--- src/org/ramadda/repository/output/JsonOutputHandler.java
+++ src/org/ramadda/repository/output/JsonOutputHandler.java
@@ -24,6 +24,8 @@ package org.ramadda.repository.output;
 import org.w3c.dom.*;
 
 import java.text.SimpleDateFormat;
+import java.text.StringCharacterIterator;
+
 import ucar.unidata.geoloc.Bearing;
 import ucar.unidata.geoloc.LatLonPointImpl;
 
@@ -31,6 +33,7 @@ import ucar.unidata.data.gis.KmlUtil;
 
 import org.ramadda.repository.*;
 import org.ramadda.repository.metadata.Metadata;
+import org.ramadda.repository.type.Column;
 import org.ramadda.repository.auth.*;
 
 import java.text.DateFormat;
@@ -56,6 +59,8 @@ import java.util.Properties;
 
 import com.google.gson.*;
 
+
+
 /**
  *
  *
@@ -64,12 +69,19 @@ import com.google.gson.*;
  */
 public class JsonOutputHandler extends OutputHandler {
 
+	// Parameters for the output
+	public static final String ARG_EXTRACOLUMNS = "extracolumns";
+	public static final String ARG_METADATA = "metadata";
+	public static final String ARG_LINKS = "links";
+	public static final String ARG_ONLYENTRY = "onlyentry";
+	
 
     /** _more_ */
     public static final OutputType OUTPUT_JSON =
         new OutputType("JSON", "json",
-                       OutputType.TYPE_NONHTML | OutputType.TYPE_FORSEARCH,   
+                       OutputType.TYPE_NONHTML | OutputType.TYPE_FORSEARCH  | OutputType.TYPE_TOOLBAR,
                        "", ICON_JSON);
+
 
 
     /**
@@ -129,9 +141,12 @@ public class JsonOutputHandler extends OutputHandler {
             throws Exception {
 
         List<Entry> allEntries = new ArrayList<Entry>();
-        //        allEntries.add(group);
-        allEntries.addAll(subGroups);
-        allEntries.addAll(entries);
+        if(request.get(ARG_ONLYENTRY, false))
+        		allEntries.add(group);
+        else{
+	        allEntries.addAll(subGroups);
+	        allEntries.addAll(entries);
+        }
         StringBuffer sb = new StringBuffer();
         sb.append("[");
         int cnt=0;
@@ -139,11 +154,11 @@ public class JsonOutputHandler extends OutputHandler {
             if(cnt>0)
                 sb.append(",");
             cnt++;
-            toJson(entry, sb);
+            toJson(request,entry, sb);
+            
             sb.append("\n");
         }
         sb.append("]");
-
 
         return new Result("", sb, "application/json");
     }
@@ -184,15 +199,20 @@ public class JsonOutputHandler extends OutputHandler {
         }
     }
 
-    private void toJson(Entry entry, StringBuffer sb) throws Exception {
+
+    private void toJson(Request request, Entry entry, StringBuffer sb) throws Exception {
         sb.append("{");
+        
         attr(sb, "id", qt(entry.getId()), false);
-        qtattr(sb, "name", entry.getName());
-        qtattr(sb, "description", entry.getDescription());
+        qtattr(sb, "name", replaceForJSON(entry.getName()));
+        qtattr(sb, "description", replaceForJSON(entry.getDescription()));
         qtattr(sb, "type", entry.getType());
+        
         if(entry.isGroup()) {
             attr(sb, "isGroup", "true");
-        }
+        }else attr(sb, "isGroup", "false");
+        
+        qtattr(sb, "icon", getRepository().getEntryManager().getIconUrl(request, entry));
         qtattr(sb, "parent", entry.getParentEntryId());
         qtattr(sb, "user", entry.getUser().getId());
         qtattr(sb, "createDate", fmt(entry.getCreateDate()));
@@ -201,57 +221,146 @@ public class JsonOutputHandler extends OutputHandler {
 
         if(entry.hasNorth())
             attr(sb, "north", ""+entry.getNorth());
+        else 
+        	attr(sb, "north", "-9999");
+        
         if(entry.hasSouth())
             attr(sb, "south", ""+entry.getSouth());
+        else 
+        	attr(sb, "south", "-9999");
+        
         if(entry.hasEast())
             attr(sb, "east", ""+entry.getEast());
+        else 
+        	attr(sb, "east", "-9999");
+        
         if(entry.hasWest())
             attr(sb, "west", ""+entry.getWest());
+        else 
+        	attr(sb, "west", "-9999");
+        
         if(entry.hasAltitudeTop())
             attr(sb, "altitudeTop", ""+entry.getAltitudeTop());
+        else 
+        	attr(sb, "altitudeTop", "-9999");
+        
         if(entry.hasAltitudeBottom())
             attr(sb, "altitudeBottom", ""+entry.getAltitudeBottom());
-
+        else 
+        	attr(sb, "altitudeBottom", "-9999");
+        
+        
 
         Resource resource = entry.getResource();
+        
         //TODO: add services
         if(resource!=null) {
             if(resource.isUrl()) {
-                qtattr(sb, "url", resource.getPath());
+                qtattr(sb, "filename", replaceForJSON(resource.getPath()));
+                attr(sb, "filesize", ""+resource.getFileSize());
+                qtattr(sb, "md5", "");
+           //TODO MATIAS            } else if(resource.isFileNoCheck()) {
             } else if(resource.isFile()) {
                 qtattr(sb, "filename", getStorageManager().getFileTail(entry));
-                if(resource.getFileSize()>0) {
-                    attr(sb, "filesize", ""+resource.getFileSize());
-                }
+                attr(sb, "filesize", ""+resource.getFileSize());
                 if(resource.getMd5()!=null) {
                     qtattr(sb, "md5", resource.getMd5());
-                }
+                }else qtattr(sb, "md5", "");
             }
+        }else {
+        	qtattr(sb, "filename", "no resource");
+            attr(sb, "filesize", "");
+            qtattr(sb, "md5", "");
         }
-        List<Metadata> metadataItems= getMetadataManager().getMetadata(entry);
-        if(metadataItems!=null && metadataItems.size()>0) {
-            StringBuffer msb = new StringBuffer("[");
-            for(int j=0;j<metadataItems.size();j++) {
-                if(j>0) msb.append(",\n ");
-                Metadata metadata = metadataItems.get(j);
-                msb.append("{");
-                qtattr(msb, "id", metadata.getId(), false);
-                qtattr(msb, "type", metadata.getType());
-                int attrIdx=1;
-                while(true) {
-                    String attr = metadata.getAttr(attrIdx);
-                    if(attr==null) break;
-                    if(attr.length()>0) {
-                        qtattr(msb, "attr"+attrIdx, attr);
-                    }
-                    attrIdx++;
-                }
-                msb.append("}\n");
-            }
-            msb.append("]");
-            attr(sb,"metadata",msb.toString());
+        
+        
+        // Add special columns to the entries depending on the type
+        if(request.get(ARG_EXTRACOLUMNS, true)){
+	        Object[] extraParameters = entry.getValues();
+	        if (extraParameters != null){
+	        	StringBuffer msb = new StringBuffer("[");
+	        	for(int i=0;i<extraParameters.length;i++){
+	        		if(i>0) msb.append(",\n ");
+	        		msb.append("{");
+	        		String value = entry.getValue(i, "");
+	        		// I don't know how to extract column name maybe there is a function but
+	        		// I don't know so I add the column position. In any case you need to know
+	        		// the output type in the client side to deal with the parameters.
+	        		qtattr(msb, Integer.toString(i), replaceForJSON(value), false);
+	        		
+	        		 msb.append("}");
+	    		}
+	        	 msb.append("]");
+	             attr(sb,"extraColumns",msb.toString());
+	        }else{
+	        	attr(sb,"extraColumns","[]");
+	        }
         }
-        sb.append("}\n");
+       
+        
+        
+        if(request.get(ARG_LINKS, false)){
+	        List<Link> links = new ArrayList<Link>();
+	        links = repository.getEntryManager().getEntryLinks(request, entry);
+	        if (links!=null){
+	        	StringBuffer msb = new StringBuffer("[");
+	        	int index=0;
+		        for(Link link: links) {
+		        	if(index>0) msb.append(",\n ");
+	        		if(link.getLabel()!=""){
+	        			msb.append("{");
+		        		qtattr(msb, "label", replaceForJSON(link.getLabel()), false);
+		        		OutputType outputType=link.getOutputType();
+		        		if(outputType==null)
+		        			qtattr(msb, "type", "unknow");
+		        		else 
+		        			qtattr(msb, "type", replaceForJSON(outputType.toString()));
+		        		
+		        		qtattr(msb, "url", link.getUrl());
+		        		qtattr(msb, "icon", link.getIcon());
+		        		msb.append("}");
+		        	   	index++;
+	        		}
+		        }
+		        msb.append("]");
+	            attr(sb,"links",msb.toString());
+	        }else{
+	        	attr(sb,"links","[]");
+	        }
+        }
+        
+        if(request.get(ARG_METADATA, true)){
+	        List<Metadata> metadataItems= getMetadataManager().getMetadata(entry);
+	        if(metadataItems!=null && metadataItems.size()>0) {
+	            StringBuffer msb = new StringBuffer("[");
+	            for(int j=0;j<metadataItems.size();j++) {
+	                if(j>0) msb.append(",\n ");
+	                Metadata metadata = metadataItems.get(j);
+	                msb.append("{");
+	                qtattr(msb, "id", metadata.getId(), false);
+	                qtattr(msb, "type", metadata.getType());
+	                int attrIdx=1;
+	                //We always add the four attributes to have always the same structure
+	                while(attrIdx<=4) {
+	                    String attr = metadata.getAttr(attrIdx);
+	                    if(attr!=null){
+		                    if(attr.length()>0) {
+		                        qtattr(msb, "attr"+attrIdx, replaceForJSON(attr));
+		                    }else{
+		                    	qtattr(msb, "attr"+attrIdx, "");
+		                    }
+	                    }else qtattr(msb, "attr"+attrIdx, "");
+	                    attrIdx++;
+	                }
+	                msb.append("}\n");
+	            }
+	            msb.append("]");
+	            attr(sb,"metadata",msb.toString());
+	        }else 
+	        	attr(sb,"metadata","[]");
+        }
+	    sb.append("}\n");
+	        
         
     }
 
@@ -286,6 +395,46 @@ public class JsonOutputHandler extends OutputHandler {
             return false;
         }
     }
-
+    
+    public static String replaceForJSON(String aText){
+    	if(aText==null || aText==""){return aText;}
+    	final StringBuilder result = new StringBuilder();
+        
+        StringCharacterIterator iterator = new StringCharacterIterator(aText);
+        char character = iterator.current();
+        while (character != StringCharacterIterator.DONE){
+          if( character == '\"' ){
+            result.append("\\\"");
+          }
+          else if(character == '\\'){
+            result.append("\\\\");
+          }
+          else if(character == '/'){
+            result.append("\\/");
+          }
+          else if(character == '\b'){
+            result.append("\\b");
+          }
+          else if(character == '\f'){
+            result.append("\\f");
+          }
+          else if(character == '\n'){
+            result.append("\\n");
+          }
+          else if(character == '\r'){
+            result.append("\\r");
+          }
+          else if(character == '\t'){
+            result.append("\\t");
+          }
+          else {
+            //the char is not a special one
+            //add it to the result as is
+            result.append(character);
+          }
+          character = iterator.next();
+        }
+        return result.toString();    
+      }
 
 }
