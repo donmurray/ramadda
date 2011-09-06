2d9a8c2e238104e1910851ac9c86ae1a705e9602
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-09-06 12:04:01 +0000

    more tweaks

--- src/org/ramadda/repository/release/ramadda.sh
+++ src/org/ramadda/repository/release/ramadda.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-## To run RAMADDA stand-alone just do
-echo 'Running RAMADDA on port 8080'
-echo 'Access it at: http://localhost:8080/repository'
-java -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 $*
+JAVA=java
+
+echo 'RAMADDA: running at http://localhost:8080/repository'
+
+${JAVA} -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 $*
 
 
-
-##This will create a directory under ~/.ramadda to store content and the database
-#To change the directory do:
-##java -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 -Dramadda_home=/some/other/directory
+##RAMADDA will create a home directory under ~/.ramadda to store content 
+##and the database. To change the directory do:
+##${JAVA} -Xmx512m -XX:MaxPermSize=256m -jar lib/@REPOSITORYJAR@ -port 8080 -Dramadda_home=/some/other/directory
 
 
 #The default above is to use Java Derby as the database
 #To run with mysql you do:
-#java -Xmx512m -XX:MaxPermSize=256m -jar @REPOSITORYJAR@ -Dramadda.db=mysql
+#${JAVA} -Xmx512m -XX:MaxPermSize=256m -jar @REPOSITORYJAR@ -Dramadda.db=mysql
 
 
 #For more information see:
-#http://facdev.unavco.org/repository/help/installing.html
+#http://facdev.unavco.org/repository/userguide/installing.html
 
 
 
