48a76ebc56c57925ae8fcee01191b107c025ae37
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-09-27 09:59:44 +0000

    argument check

--- bin/sfrelease.sh
+++ bin/sfrelease.sh
@@ -3,18 +3,28 @@
 # sfrelease.sh <sf user id>
 #
 
+sfuser=${1}
+
+if test -z ${sfuser} ; then 
+    echo "error: missing argument"
+    echo "usage: sfrelease.sh <sf user id>"
+    exit;
+fi
+
+
+
 #Make the release
 ant release
 
 #scp the top level build products to SF
 
-scp dist/repository.war dist/ramadda1.3.zip  dist/allplugins.zip dist/geodataplugins.zip dist/repositoryclient.zip dist/repositoryclient.jar ${1},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda1.3
+scp dist/repository.war dist/ramadda1.3.zip  dist/allplugins.zip dist/geodataplugins.zip dist/repositoryclient.zip dist/repositoryclient.jar ${sfuser},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda1.3
 
 #now make the rest of the plugins
 ant otherplugins
 
 #scp all of the plugins over to SF
 #This will unfortunately prompt for a password
-scp dist/plugins/* ${1},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda1.3/plugins
+scp dist/plugins/* ${sfuser},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda1.3/plugins
 
 
