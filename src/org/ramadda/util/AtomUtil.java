beb344965df581787ce1cfb3d7e7b20c8b2ccd1d
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2011-11-17 01:08:53 +0000

    new GML tags

--- src/org/ramadda/util/AtomUtil.java
+++ src/org/ramadda/util/AtomUtil.java
@@ -18,7 +18,6 @@
 * DEALINGS IN THE SOFTWARE.
 */
 
-
 package org.ramadda.util;
 
 
@@ -70,6 +69,16 @@ public class AtomUtil {
 
     /** _more_ */
     public static final String REL_ALTERNATE = "alternate";
+
+
+    /** _more_          */
+    public static final String TAG_GML_TIMEPERIOD = "gml:TimePeriod";
+
+    /** _more_          */
+    public static final String TAG_GML_BEGIN = "gml:begin";
+
+    /** _more_          */
+    public static final String TAG_GML_END = "gml:end";
 
 
     /** _more_ */
@@ -233,25 +242,25 @@ public class AtomUtil {
      * @return _more_
      */
     public static String openFeed(String id) {
-        
-        String blobOfNamespaces = " xmlns:gco=\"http://www.isotc211.org/2005/gco\"             xmlns:gmd=\"http://www.isotc211.org/2005/gmd\"             xmlns:gmi=\"http://www.isotc211.org/2005/gmi\" ";
-/*
-            xmlns:gmx="http://www.isotc211.org/2005/gmx" 
-            xmlns:gss="http://www.isotc211.org/2005/gss" 
-            xmlns:gts="http://www.isotc211.org/2005/gts" 
-            xmlns:gsr="http://www.isotc211.org/2005/gsr" 
-*/
+
+        String blobOfNamespaces =
+            " xmlns:gco=\"http://www.isotc211.org/2005/gco\"             xmlns:gmd=\"http://www.isotc211.org/2005/gmd\"             xmlns:gmi=\"http://www.isotc211.org/2005/gmi\"    xmlns:gml=\"http://www.isotc211.org/2005/gml\" ";
+        /*
+                    xmlns:gmx="http://www.isotc211.org/2005/gmx"
+                    xmlns:gss="http://www.isotc211.org/2005/gss"
+                    xmlns:gts="http://www.isotc211.org/2005/gts"
+                    xmlns:gsr="http://www.isotc211.org/2005/gsr"
+        */
 
 
 
-        return XmlUtil.openTag(
-            TAG_FEED,
-            blobOfNamespaces +
-            XmlUtil.attrs(
-                ATTR_XMLNS, XMLNS, ATTR_XMLNS_GEORSS,
-                XMLNS_GEORSS)) + XmlUtil.tag(TAG_ID, "", id)
-                               + XmlUtil.tag(
-                                   TAG_UPDATED, "", format(new Date()));
+        return XmlUtil.openTag(TAG_FEED,
+                               blobOfNamespaces
+                               + XmlUtil.attrs(ATTR_XMLNS, XMLNS,
+                                   ATTR_XMLNS_GEORSS,
+                                   XMLNS_GEORSS)) + XmlUtil.tag(TAG_ID, "",
+                                       id) + XmlUtil.tag(TAG_UPDATED, "",
+                                           format(new Date()));
     }
 
     /**
