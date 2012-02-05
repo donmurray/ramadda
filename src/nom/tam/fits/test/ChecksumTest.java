4aefc10c4c6679b26cf79e17c25b63d912174028
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-02-05 04:09:41 +0000

    a local copy of the nom FITS file format reading code

---
+++ src/nom/tam/fits/test/ChecksumTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nom.tam.fits.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import junit.framework.JUnit4TestAdapter;
+
+import nom.tam.fits.*;
+import nom.tam.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author tmcglynn
+ */
+public class ChecksumTest {
+
+    @Test public void testChecksum() throws Exception {
+
+        int[][] data = new int[][] {{1,2}, {3,4}, {5,6}};
+        Fits f = new Fits();
+        BasicHDU bhdu = FitsFactory.HDUFactory(data);
+        f.addHDU(bhdu);
+
+        Fits.setChecksum(bhdu);
+        ByteArrayOutputStream    bs   = new ByteArrayOutputStream();
+        BufferedDataOutputStream bdos = new BufferedDataOutputStream(bs);
+        f.write(bdos);
+        bdos.close();
+        byte[] stream = bs.toByteArray();
+        long chk = Fits.checksum(stream);
+        int val = (int)chk;
+
+        assertEquals("CheckSum test", -1, val);
+    }
+
+}
