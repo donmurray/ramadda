7e15fcc05c7b1028af738daf6136b71954e946f6
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-02-12 17:59:01 +0000

    update

---
+++ src/org/ramadda/geodata/astro/FitsApiHandler.java
@@ -0,0 +1,63 @@
+/*
+* Copyright 2008-2011 Jeff McWhirter/ramadda.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.geodata.fits;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.auth.*;
+import org.ramadda.repository.map.*;
+import org.ramadda.repository.output.*;
+import org.ramadda.repository.search.*;
+import org.ramadda.repository.type.TypeHandler;
+
+
+import org.w3c.dom.*;
+
+import java.util.Hashtable;
+
+
+/**
+ *
+ *
+ * @author Jeff McWhirter
+ * @version $Revision: 1.3 $
+ */
+
+public class FitsApiHandler extends SpecialSearch {
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param node _more_
+     * @param props _more_
+     *
+     * @throws Exception On badness
+     */
+    public FitsApiHandler(Repository repository, Element node,
+                          Hashtable props)
+            throws Exception {
+        super(repository, node, props);
+    }
+
+
+
+}
