16095640414545af212e2045108bb72e71378ebe
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-02-15 15:48:54 +0000

    new type

---
+++ src/org/ramadda/geodata/astro/HoroscopeTypeHandler.java
@@ -0,0 +1,82 @@
+/*
+* Copyright 2008-2011 Jeff McWhirter/ramadda.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.geodata.astro;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.type.*;
+
+
+import org.w3c.dom.*;
+
+
+import ucar.unidata.util.HtmlUtil;
+import ucar.unidata.util.StringUtil;
+
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ *
+ *
+ */
+public class HoroscopeTypeHandler extends GenericTypeHandler {
+
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public HoroscopeTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param group _more_
+     * @param subGroups _more_
+     * @param entries _more_
+     * @param entry _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    public Result getHtmlDisplay(Request request, Entry entry)
+            throws Exception {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<script type=\"text/javascript\" src=\"http://www.albinoblacksheep.com/horoscope/embed.js\"></script><noscript><a href=\"http://www.albinoblacksheep.com/horoscope/\">Horoscopes</a></noscript>");
+        return new Result(msg("Horoscope"), sb);
+    }
+
+
+
+
+}
