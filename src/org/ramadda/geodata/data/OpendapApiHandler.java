2f8c0e07bb59b39833e4056807cf33a73562a3b1
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-03-02 11:06:47 +0000

    snapshot

---
+++ src/org/ramadda/geodata/data/OpendapApiHandler.java
@@ -0,0 +1,90 @@
+/*
+* Copyright 2008-2011 Jeff McWhirter/ramadda.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.geodata.data;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.auth.*;
+import org.ramadda.repository.type.*;
+
+
+import org.w3c.dom.*;
+
+import ucar.unidata.sql.Clause;
+
+
+import ucar.unidata.sql.SqlUtil;
+import ucar.unidata.sql.SqlUtil;
+import ucar.unidata.util.DateUtil;
+
+import ucar.unidata.util.HtmlUtil;
+import ucar.unidata.util.HttpServer;
+import ucar.unidata.util.IOUtil;
+import ucar.unidata.util.LogUtil;
+import ucar.unidata.util.Misc;
+import ucar.unidata.util.StringUtil;
+import ucar.unidata.util.WikiUtil;
+import ucar.unidata.xml.XmlUtil;
+
+import java.sql.PreparedStatement;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
+
+/**
+ *
+ *
+ * @author IDV Development Team
+ * @version $Revision: 1.3 $
+ */
+public class OpendapApiHandler extends RepositoryManager implements RequestHandler {
+
+    /*
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public OpendapApiHandler(Repository repository, Element node, Hashtable props)
+            throws Exception {
+        super(repository);
+    }
+
+
+    public Result processOpendapRequest(Request request)
+            throws Exception {
+        return null;
+    }
+
+
+
+}
