cf86b9301b0d39f1f22d7d53073a67c3c6b760d9
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-04-25 04:07:17 +0000

    new plugin

---
+++ src/org/ramadda/plugins/biblio/BiblioOutputHandler.java
@@ -0,0 +1,148 @@
+/*
+* Copyright 2008-2011 Jeff McWhirter/ramadda.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.plugins.biblio;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.auth.*;
+import org.ramadda.repository.output.*;
+
+
+import org.w3c.dom.*;
+
+import ucar.unidata.sql.SqlUtil;
+import ucar.unidata.ui.ImageUtils;
+import ucar.unidata.util.DateUtil;
+import ucar.unidata.util.HtmlUtil;
+import ucar.unidata.util.IOUtil;
+import ucar.unidata.util.Misc;
+
+
+import ucar.unidata.util.StringUtil;
+import ucar.unidata.util.WmsUtil;
+import ucar.unidata.xml.XmlUtil;
+
+import java.io.*;
+
+
+
+
+import java.io.File;
+
+
+import java.net.*;
+
+
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+import java.util.TimeZone;
+
+
+
+import java.util.regex.*;
+
+import java.util.zip.*;
+
+
+/**
+ *
+ *
+ * @author IDV Development Team
+ * @version $Revision: 1.3 $
+ */
+public class BiblioOutputHandler extends OutputHandler {
+
+
+
+
+    /** _more_ */
+    public static final OutputType OUTPUT_BIBLIO_TEST =
+        new OutputType("Biblio Test", "biblio_test",
+                       OutputType.TYPE_VIEW, "", "/biblio/book.png");
+
+
+    /**
+     * _more_
+     *
+     *
+     * @param repository _more_
+     * @param element _more_
+     * @throws Exception _more_
+     */
+    public BiblioOutputHandler(Repository repository, Element element)
+            throws Exception {
+        super(repository, element);
+        addType(OUTPUT_BIBLIO_TEST);
+    }
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param state _more_
+     * @param links _more_
+     *
+     *
+     * @throws Exception _more_
+     */
+    public void getEntryLinks(Request request, State state, List<Link> links)
+            throws Exception {
+        if (state.entry == null) {
+            return;
+        }
+        if (state.entry.getTypeHandler().getType().equals("biblio")) {
+            links.add(makeLink(request, state.getEntry(), OUTPUT_BIBLIO_TEST));
+        }
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param outputType _more_
+     * @param group _more_
+     * @param subGroups _more_
+     * @param entries _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    @Override
+    public Result outputEntry(Request request, OutputType outputType,
+                              Entry entry) 
+            throws Exception {
+        StringBuffer sb = new StringBuffer();
+        sb.append(entry.getName());
+        return new Result("", sb);
+
+    }
+
+
+
+}
