39c0a3b2eb8bfbbaf1ce4dbbba88ad96231451b9
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-06-19 13:10:51 +0000

    snapshot

--- bin/sfrelease.sh
+++ bin/sfrelease.sh
@@ -6,9 +6,10 @@
 sfuser=${1}
 
 if test -z ${sfuser} ; then 
-    echo "error: missing argument"
-    echo "usage: sfrelease.sh <sf user id>"
-    exit;
+#    echo "error: missing argument"
+#    echo "usage: sfrelease.sh <sf user id>"
+#    exit;
+     sfuser=jmcwhirter
 fi
 
 
@@ -18,13 +19,13 @@ ant release
 
 #scp the top level build products to SF
 
-scp dist/repository.war dist/ramadda1.3.zip  dist/allplugins.zip dist/geodataplugins.zip dist/repositoryclient.zip dist/repositoryclient.jar ${sfuser},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda1.3
+scp dist/repository.war dist/ramadda${RAMADDA_VERSION}.zip  dist/allplugins.zip dist/repositoryclient.zip dist/repositoryclient.jar ${sfuser},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda${RAMADDA_VERSION}
 
 #now make the rest of the plugins
 ant otherplugins
 
 #scp all of the plugins over to SF
 #This will unfortunately prompt for a password
-scp dist/plugins/* ${sfuser},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda1.3/plugins
+scp dist/plugins/* ${sfuser},ramadda@frs.sourceforge.net:/home/frs/project/r/ra/ramadda/ramadda${RAMADDA_VERSION}/plugins
 
 
