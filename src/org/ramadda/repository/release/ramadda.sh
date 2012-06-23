8f21e277509e753d743bc8d80f5eb28d4a5e2ed8
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-06-23 22:23:57 +0000

    up the memory

--- src/org/ramadda/repository/release/ramadda.sh
+++ src/org/ramadda/repository/release/ramadda.sh
@@ -3,19 +3,20 @@
 RAMADDADIR=`dirname $0`
 JAVA=java
 
+MEM=1024m
 echo 'RAMADDA: running at http://localhost:8080/repository'
 
-${JAVA} -Xmx512m -XX:MaxPermSize=256m -Dfile.encoding=utf-8 -jar ${RAMADDADIR}/lib/ramadda.jar -port 8080 $*
+${JAVA} -Xmx${MEM} -XX:MaxPermSize=256m -Dfile.encoding=utf-8 -jar ${RAMADDADIR}/lib/ramadda.jar -port 8080 $*
 
 
 ##RAMADDA will create a home directory under ~/.ramadda to store content 
 ##and the database. To change the directory do:
-##${JAVA} -Xmx512m -XX:MaxPermSize=256m -jar lib/ramadda.jar -port 8080 -Dramadda_home=/some/other/directory
+##${JAVA} -Xmx{MEM} -XX:MaxPermSize=256m -jar lib/ramadda.jar -port 8080 -Dramadda_home=/some/other/directory
 
 
 #The default above is to use Java Derby as the database
 #To run with mysql you do:
-#${JAVA} -Xmx512m -XX:MaxPermSize=256m -jar ramadda.jar -Dramadda.db=mysql
+#${JAVA} -Xmx{MEM} -XX:MaxPermSize=256m -jar ramadda.jar -Dramadda.db=mysql
 
 
 #For more information see:
