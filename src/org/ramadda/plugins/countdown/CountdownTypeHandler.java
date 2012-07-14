e758a65a9d9b42e66d5d594455af58edff100809
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-07-14 12:38:38 +0000

    new plugin - countdown clock

---
+++ src/org/ramadda/plugins/countdown/CountdownTypeHandler.java
@@ -0,0 +1,94 @@
+/*
+* Copyright 2008-2011 Jeff McWhirter/ramadda.org
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.plugins.countdown;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.output.*;
+import org.ramadda.repository.type.*;
+
+import org.w3c.dom.*;
+
+import org.ramadda.util.HtmlUtils;
+import ucar.unidata.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.util.List;
+
+
+/**
+ *
+ *
+ */
+public class CountdownTypeHandler extends GenericTypeHandler {
+
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public CountdownTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+    private int countdownCnt =0;
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param group _more_
+     * @param subGroups _more_
+     * @param entries _more_
+     * @param entry _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    public Result getHtmlDisplay(Request request, Entry entry)
+            throws Exception {
+        String orient = entry.getValue(0,"");
+        String howMany = entry.getValue(1,"");
+        if(howMany.length()==0) howMany = "4";
+        StringBuffer sb = new StringBuffer();
+        Date now = new Date();
+        Date to  = new Date(entry.getStartDate());
+        long diff = to.getTime()-now.getTime()/1000;
+        String id = "countdownid_" + (countdownCnt++);
+        //        sb.append(HtmlUtils.cssBlock(".countdown-clock {font-size: 150%;}\n.countdown-number {color:#A94DEA;\n.countdown-label {color:#000;}\n"));
+        String inner = HtmlUtils.div("", HtmlUtils.id(id)+HtmlUtils.cssClass("countdown-clock"));
+        sb.append("<table><td><td>" +HtmlUtils.div(inner,HtmlUtils.cssClass("countdown"))+"</td></tr></table>");
+        sb.append(HtmlUtils.script("$(document).ready(function() {countdownStart(" + HtmlUtils.squote(entry.getName())+","+HtmlUtils.squote(id)+"," + (to.getTime()/1000)+"," + HtmlUtils.squote(orient) +"," +howMany +");});\n"));
+        return new Result("Countdown", sb);
+    }
+
+
+
+
+}
