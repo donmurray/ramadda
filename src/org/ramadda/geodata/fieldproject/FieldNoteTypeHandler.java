6e7be98f96057a14cebc9e192e9c40cec9ec158b
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2012-11-13 19:28:21 +0000

    new field note type, etc

---
+++ src/org/ramadda/geodata/fieldproject/FieldNoteTypeHandler.java
@@ -0,0 +1,119 @@
+/*
+* Copyright 2008-2012 Jeff McWhirter/ramadda.org
+*                     Don Murray/CU-CIRES
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.geodata.fieldproject;
+
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.type.*;
+
+import org.w3c.dom.*;
+
+import java.text.SimpleDateFormat;
+import java.io.File;
+import java.util.TimeZone;
+
+
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ *
+ *
+ * @author Jeff McWhirter
+ */
+public class FieldNoteTypeHandler extends ExtensibleGroupTypeHandler {
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param node _more_
+     * @throws Exception On badness
+     */
+    public FieldNoteTypeHandler(Repository repository, Element node)
+            throws Exception {
+        super(repository, node);
+    }
+
+
+   @Override
+   public String getFormDefault(Entry entry, String arg, String dflt) {
+        if(arg.equals(ARG_NAME)) {
+            Date now = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM d yyyy");
+            sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+            return sdf.format(now) +" - ";
+        }
+        return super.getFormDefault(entry, arg, dflt);
+    }
+
+
+    public Date[] getDefaultDateRange(Request request, Entry entry) {
+        if(entry!=null) return super.getDefaultDateRange(request, entry);
+        return new Date[]{new Date(), null};
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param entry _more_
+     * @param parent _more_
+     * @param newEntry _more_
+     *
+     * @throws Exception On badness
+     */
+    @Override
+    public void initializeEntryFromForm(Request request, Entry entry,
+                                        Entry parent, boolean newEntry)
+            throws Exception {
+        super.initializeEntryFromForm(request, entry, parent, newEntry);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param entry _more_
+     *
+     * @throws Exception On badness
+     */
+    @Override
+    public void initializeNewEntry(Entry entry) throws Exception {
+        super.initializeNewEntry(entry);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param newEntry _more_
+     */
+    @Override
+    public void doFinalInitialization(Request request, Entry newEntry) {
+        super.doFinalInitialization(request, newEntry);
+    }
+
+}
