5f2a5627687aedbaed51900e4bd5fa7bf99346fa
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-02-11 23:42:27 +0000

    Add some handling of the info results. etc

--- src/org/ramadda/geodata/cdmdata/CDOOutputHandler.java
+++ src/org/ramadda/geodata/cdmdata/CDOOutputHandler.java
@@ -1,23 +1,23 @@
 /*
-* Copyright 2008-2012 Jeff McWhirter/ramadda.org
-*                     Don Murray/CU-CIRES
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
-* software and associated documentation files (the "Software"), to deal in the Software 
-* without restriction, including without limitation the rights to use, copy, modify, 
-* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
-* permit persons to whom the Software is furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all copies 
-* or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
-* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-* DEALINGS IN THE SOFTWARE.
-*/
+ * Copyright 2008-2012 Jeff McWhirter/ramadda.org
+ *                     Don Murray/CU-CIRES
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, 
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
 package org.ramadda.geodata.cdmdata;
 
 
@@ -80,13 +80,13 @@ public class CDOOutputHandler extends OutputHandler {
 
     /** info types */
     @SuppressWarnings("unchecked")
-    List<TwoFacedObject> INFO_TYPES = Misc.toList(new Object[] {
-                                          new TwoFacedObject("Info", OP_INFO),
-                                          new TwoFacedObject(
-                                              "Short Info",
-                                              OP_SINFO), new TwoFacedObject(
-                                                  "Number of Years",
-                                                  OP_NYEAR), });
+        List<TwoFacedObject> INFO_TYPES = Misc.toList(new Object[] {
+                new TwoFacedObject("Info", OP_INFO),
+                new TwoFacedObject(
+                                   "Short Info",
+                                   OP_SINFO), new TwoFacedObject(
+                                                                 "Number of Years",
+                                                                 OP_NYEAR), });
 
 
     /** the product directory */
@@ -103,7 +103,7 @@ public class CDOOutputHandler extends OutputHandler {
      * @throws Exception   problem creating handler
      */
     public CDOOutputHandler(Repository repository, Element element)
-            throws Exception {
+        throws Exception {
         super(repository, element);
         addType(OUTPUT_CDO);
         cdoPath = getProperty(PROP_CDO_PATH, null);
@@ -130,12 +130,12 @@ public class CDOOutputHandler extends OutputHandler {
      * @throws Exception Problem adding links
      */
     public void getEntryLinks(Request request, State state, List<Link> links)
-            throws Exception {
+        throws Exception {
         if ( !haveCdo()) {
             return;
         }
         if ((state.entry != null) && state.entry.isFile()
-                && state.entry.getResource().getPath().endsWith(".nc")) {
+            && state.entry.getResource().getPath().endsWith(".nc")) {
             links.add(makeLink(request, state.entry, OUTPUT_CDO));
         }
     }
@@ -145,6 +145,7 @@ public class CDOOutputHandler extends OutputHandler {
      *
      * @return  the directory
      *
+
      * @throws Exception  problem getting directory
      */
     private File getProductDir() throws Exception {
@@ -159,10 +160,9 @@ public class CDOOutputHandler extends OutputHandler {
     }
 
 
-    @Override
     public CdmDataOutputHandler getDataOutputHandler() throws Exception {
         return (CdmDataOutputHandler) getRepository().getOutputHandler(
-            CdmDataOutputHandler.OUTPUT_OPENDAP.toString());
+                                                                       CdmDataOutputHandler.OUTPUT_OPENDAP.toString());
     }
 
 
@@ -181,47 +181,50 @@ public class CDOOutputHandler extends OutputHandler {
      */
     public Result outputEntry(Request request, OutputType outputType,
                               Entry entry)
-            throws Exception {
+        throws Exception {
 
 
         if (request.defined(ARG_SUBMIT)) {
             return outputCDO(request, entry);
         }
+        StringBuffer sb      = new StringBuffer();
+        addForm(request, entry, sb);
+        return new Result("CDO Form", sb);
+    }
 
+    private void   addForm(Request request, Entry entry, StringBuffer sb) throws Exception {
         CdmDataOutputHandler dataOutputHandler = getDataOutputHandler();
         NetcdfDataset dataset =
             NetcdfDataset.openDataset(entry.getResource().getPath());
         dataset.close();
 
 
-        StringBuffer sb      = new StringBuffer("");
         String       formUrl = request.url(getRepository().URL_ENTRY_SHOW);
         sb.append(HtmlUtils.form(formUrl,
                                  makeFormSubmitDialog(sb,
-                                     msg("Apply CDO..."))));
+                                                      msg("Apply CDO..."))));
 
         String buttons = HtmlUtils.submit("Extract Data", ARG_SUBMIT);
         sb.append(buttons);
         sb.append(" ");
         sb.append(
-            HtmlUtils.href(
-                "https://code.zmaw.de/projects/cdo/wiki/Cdo#Documentation",
-                "CDO Documentation", " target=_external "));
+                  HtmlUtils.href(
+                                 "https://code.zmaw.de/projects/cdo/wiki/Cdo#Documentation",
+                                 "CDO Documentation", " target=_external "));
 
         sb.append(HtmlUtils.formTable());
         sb.append(HtmlUtils.hidden(ARG_OUTPUT, OUTPUT_CDO));
         sb.append(HtmlUtils.hidden(ARG_ENTRYID, entry.getId()));
 
         addInfoWidget(request, sb);
-
         addPublishWidget(
-            request, entry, sb,
-            msg("Select a folder to publish the generated NetCDF file to"));
+                         request, entry, sb,
+                         msg("Select a folder to publish the generated NetCDF file to"));
         sb.append(HtmlUtils.formTableClose());
         sb.append(buttons);
 
-        return new Result("CDO Form", sb);
     }
+
 
     /**
      * Add the info widget
@@ -232,7 +235,7 @@ public class CDOOutputHandler extends OutputHandler {
     private void addInfoWidget(Request request, StringBuffer sb) {
         sb.append(HtmlUtils.formEntry(msgLabel("Information"),
                                       HtmlUtils.select(ARG_CDO_OPERATION,
-                                          INFO_TYPES)));
+                                                       INFO_TYPES)));
     }
 
 
@@ -252,10 +255,13 @@ public class CDOOutputHandler extends OutputHandler {
         String newName = IOUtil.stripExtension(tail) + "_product.nc";
         tail = getStorageManager().getStorageFileName(tail);
         File         outFile = new File(IOUtil.joinDir(getProductDir(),
-                                   tail));
+                                                       tail));
         List<String> commands = new ArrayList<String>();
         commands.add(cdoPath);
-        commands.add(request.getString(ARG_CDO_OPERATION, OP_INFO));
+        String operation  = request.getString(ARG_CDO_OPERATION, OP_INFO);
+
+
+        commands.add(operation);
 
         System.err.println("cmds:" + commands);
 
@@ -273,38 +279,63 @@ public class CDOOutputHandler extends OutputHandler {
         int result = process.waitFor();
         if (errorMsg.length() > 0) {
             return new Result(
-                "CDO-Error",
-                new StringBuffer(
-                    getRepository().showDialogError(
-                        "An error occurred:<br>" + errorMsg)));
-        }
-        if (outMsg.length() > 0) {
-            return new Result(
-                "CDO-Error",
-                new StringBuffer(
-                    getRepository().showDialogError(
-                        "An error occurred:<br>" + outMsg)));
+                              "CDO-Error",
+                              new StringBuffer(
+                                               getRepository().showDialogError(
+                                                                               "An error occurred:<br>" + errorMsg)));
         }
 
-        if ( !outFile.exists()) {
-            return new Result(
-                "CDO-Error",
-                new StringBuffer(
-                    getRepository().showDialogError(
-                        "Humm, the CDO generation failed for some reason")));
+        //The jeff is here for when I have a fake cdo.sh
+        boolean jeff = true;
+
+        if(!jeff) {
+            if (outMsg.length() > 0) {
+                return new Result(
+                                  "CDO-Error",
+                                  new StringBuffer(
+                                                   getRepository().showDialogError(
+                                                                                   "An error occurred:<br>" + outMsg)));
+            }
+        
+            if ( !outFile.exists()) {
+                return new Result(
+                                  "CDO-Error",
+                                  new StringBuffer(
+                                                   getRepository().showDialogError(
+                                                                                   "Humm, the CDO generation failed for some reason")));
+            }
         }
 
         if (doingPublish(request)) {
             if ( !request.defined(ARG_PUBLISH_NAME)) {
                 request.put(ARG_PUBLISH_NAME, newName);
             }
-
             return getEntryManager().processEntryPublish(request, outFile,
-                    null, entry, "generated from");
+                                                         null, entry, "generated from");
         }
 
+        //Assuming this is some text
+        if(operation.equals(OP_INFO)) {
+            String info;
+
+            if(!jeff) {
+                info = IOUtil.readInputStream(
+                                              getStorageManager().getFileInputStream(outFile));
+            } else {
+                info = outMsg;
+            }
+
+            StringBuffer sb = new StringBuffer();
+            addForm(request, entry, sb);
+            sb.append(header(msg("CDO Information")));
+            sb.append(HtmlUtils.pre(info));
+            return new Result("CDO", sb);
+        }
+
+
+
         return request.returnFile(
-            outFile, getStorageManager().getFileTail(outFile.toString()));
+                                  outFile, getStorageManager().getFileTail(outFile.toString()));
     }
 
 }
