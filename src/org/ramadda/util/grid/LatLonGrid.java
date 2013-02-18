0911f3be2af969111b42c1ea75ec2090b7d990c4
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-02-18 12:50:48 +0000

    snapshot of bounding polygon work

--- src/org/ramadda/util/grid/LatLonGrid.java
+++ src/org/ramadda/util/grid/LatLonGrid.java
@@ -31,6 +31,11 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.*;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 
 import java.io.File;
 
@@ -138,6 +143,7 @@ public class LatLonGrid extends Grid {
         if (countGrid == null) {
             return polygon;
         }
+	ArrayList<Point> points = new ArrayList<Point>();
         int firstCol = -1;
         int lastRow  = -1;
         int lastCol  = 0;
@@ -150,10 +156,13 @@ public class LatLonGrid extends Grid {
                     lastCol = col;
                     polygon.add(new double[] { getLatitude(row),
                             getLongitude(col) });
+                    //                    points.add(new Point(col,row));
                     break;
                 }
             }
         }
+
+
 
 
         lastRow = getHeight() - 1;
@@ -163,6 +172,7 @@ public class LatLonGrid extends Grid {
                     lastRow = row;
                     polygon.add(new double[] { getLatitude(row),
                             getLongitude(col) });
+                    //                    points.add(new Point(col,row));
                     break;
                 }
             }
@@ -174,6 +184,7 @@ public class LatLonGrid extends Grid {
                     lastCol = col;
                     polygon.add(new double[] { getLatitude(row),
                             getLongitude(col) });
+                    //                    points.add(new Point(col, row));
                     break;
                 }
             }
@@ -183,10 +194,23 @@ public class LatLonGrid extends Grid {
                 if (countGrid[row][col] != 0) {
                     polygon.add(new double[] { getLatitude(row),
                             getLongitude(col) });
+                    //                    points.add(new Point(col,row));
                     break;
                 }
             }
         }
+
+
+        /*
+        points  = FastConvexHull.execute(points);
+        polygon = new ArrayList<double[]>();
+        for(Point p: points) {
+            int col = p.x;
+            int row = p.y;
+            polygon.add(new double[] { getLatitude(row),
+                                       getLongitude(col) });
+        }
+        */
         return polygon;
     }
 
@@ -959,6 +983,76 @@ public class LatLonGrid extends Grid {
             theGrid[y][x] = sum / cnt;
         }
         return true;
+    }
+
+ 
+    //Copied from http://code.google.com/p/convex-hull/source/checkout
+    public static class FastConvexHull {
+
+	public static ArrayList<Point> execute(ArrayList<Point> points) {
+            ArrayList<Point> xSorted = (ArrayList<Point>) points.clone();
+            Collections.sort(xSorted, new XCompare());
+		
+            int n = xSorted.size();
+		
+            Point[] lUpper = new Point[n];
+		
+            lUpper[0] = xSorted.get(0);
+            lUpper[1] = xSorted.get(1);
+		
+            int lUpperSize = 2;
+		
+            for (int i = 2; i < n; i++) {
+                lUpper[lUpperSize] = xSorted.get(i);
+                lUpperSize++;
+			
+                while (lUpperSize > 2 && !rightTurn(lUpper[lUpperSize - 3], lUpper[lUpperSize - 2], lUpper[lUpperSize - 1])) {
+                    // Remove the middle point of the three last
+                    lUpper[lUpperSize - 2] = lUpper[lUpperSize - 1];
+                    lUpperSize--;
+                }
+            }
+		
+            Point[] lLower = new Point[n];
+		
+            lLower[0] = xSorted.get(n - 1);
+            lLower[1] = xSorted.get(n - 2);
+		
+            int lLowerSize = 2;
+		
+            for (int i = n - 3; i >= 0; i--) {
+                lLower[lLowerSize] = xSorted.get(i);
+                lLowerSize++;
+			
+                while (lLowerSize > 2 && !rightTurn(lLower[lLowerSize - 3], lLower[lLowerSize - 2], lLower[lLowerSize - 1])) {
+                    // Remove the middle point of the three last
+                    lLower[lLowerSize - 2] = lLower[lLowerSize - 1];
+                    lLowerSize--;
+                }
+            }
+		
+            ArrayList<Point> result = new ArrayList<Point>();
+		
+            for (int i = 0; i < lUpperSize; i++) {
+                result.add(lUpper[i]);
+            }
+		
+            for (int i = 1; i < lLowerSize - 1; i++) {
+                result.add(lLower[i]);
+            }
+		
+            return result;
+	}
+	
+	private static  boolean rightTurn(Point a, Point b, Point c) {
+            return (b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x) > 0;
+	}
+
+	private static class XCompare implements Comparator<Point> {
+            public int compare(Point o1, Point o2) {
+                return (new Integer(o1.x)).compareTo(new Integer(o2.x));
+            }
+	}
     }
 
 
