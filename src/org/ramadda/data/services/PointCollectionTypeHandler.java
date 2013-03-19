2abceeb071e005e2636ef88e61a8131f07dd0ab0
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-03-19 15:21:04 +0000

    snapshot

--- src/org/ramadda/data/services/PointCollectionTypeHandler.java
+++ src/org/ramadda/data/services/PointCollectionTypeHandler.java
@@ -32,10 +32,7 @@ import java.util.List;
  * @author Jeff McWhirter
  * @version $Revision: 1.3 $
  */
-public abstract class PointCollectionTypeHandler extends RecordCollectionTypeHandler {
-
-
-
+public  class PointCollectionTypeHandler extends RecordCollectionTypeHandler {
 
 
     /**
@@ -51,108 +48,8 @@ public abstract class PointCollectionTypeHandler extends RecordCollectionTypeHan
     }
 
 
-    public PointOutputHandler getPointOutputHandler() {
-        return (PointOutputHandler)getRecordOutputHandler();
-    }
-
-
-    /**
-     * _more_
-     *
-     * @param request _more_
-     * @param entry _more_
-     * @param map _more_
-     *
-     * @return _more_
-     */
-    @Override
-    public boolean addToMap(Request request, Entry entry, MapInfo map) {
-        try {
-            getPointOutputHandler().addToMap(request, entry, map);
-            return true;
-        } catch (Exception exc) {
-            throw new RuntimeException(exc);
-        }
-    }
-
-
-
-
-    /**
-     * add the list of point services
-     *
-     * @param request the request
-     * @param entry the entry
-     * @param services _more_
-     *
-     * @return list of services
-     */
-    @Override
-    public void getServices(Request request, Entry entry,
-                            List<Service> services) {
-        super.getServices(request, entry, services);
-
-        String dfltBbox = entry.getWest() + "," + entry.getSouth() + ","
-                          + entry.getEast() + "," + entry.getNorth();
-        String     url;
-        String[][] values = {
-            { getPointOutputHandler().OUTPUT_LATLONALTCSV.toString(),
-              "Lat/Lon/Alt CSV", ".csv", RecordConstants.ICON_POINTS },
-            { getPointOutputHandler().OUTPUT_LAS.toString(), "LAS", ".las", null },
-            { getPointOutputHandler().OUTPUT_ASC.toString(), "ARC Ascii Grid",
-              ".asc", null },
-            { getPointOutputHandler().OUTPUT_KMZ.toString(), "Google Earth KMZ",
-              ".kmz", getIconUrl(request, ICON_KML) },
-            { getPointOutputHandler().OUTPUT_POINTCOUNT.toString(), "Point Count",
-              ".xml", null },
-        };
-
-        for (String[] tuple : values) {
-            String product = tuple[0];
-            String name    = tuple[1];
-            String suffix  = tuple[2];
-            String icon    = tuple[3];
-            String tail    = HtmlUtils.urlEncodeExceptSpace(
-                              entry.getName()).replaceAll(" ", "_") + suffix;
-
-            url = HtmlUtils.url(getRepository().URL_ENTRY_SHOW + "/" + tail,
-                                new String[] {
-                ARG_ENTRYID, entry.getId(), ARG_OUTPUT,
-                getPointOutputHandler().OUTPUT_PRODUCT.getId(), ARG_PRODUCT, product,
-                RecordConstants.ARG_ASYNCH, "true",
-                //                RecordConstants.ARG_RECORD_SKIP,
-                //                macro(RecordConstants.ARG_RECORD_SKIP), ARG_BBOX,
-                //                macro(ARG_BBOX), ARG_DEFAULTBBOX, dfltBbox
-            }, false);
-            services.add(new Service(product, name,
-                                     request.getAbsoluteUrl(url), icon));
-        }
-
-
-        try {
-            for (Metadata metadata :
-                    getMetadataManager().getMetadata(entry)) {
-                if (metadata.getType().equals(METADATA_URL)) {
-                    //attr1=url, attr2=label, attr3=rel, attr4=mime type
-                    services.add(new Service(metadata.getAttr3(),
-                                             metadata.getAttr2(),
-                                             metadata.getAttr1(), null,
-                                             metadata.getAttr4()));
-                }
-            }
-            //Gack, cut-and-paste, add something to metadatamgr sometime
-            for (Metadata metadata :
-                     getMetadataManager().getInheritedMetadata(request, entry)) {
-                if (metadata.getType().equals(METADATA_URL)) {
-                    //attr3= type, attr2=label, attr1=url
-                    services.add(new Service(metadata.getAttr3(),
-                                             metadata.getAttr2(),
-                                             metadata.getAttr1()));
-                }
-            }
-        } catch (Exception exc) {
-            throw new RuntimeException(exc);
-        }
+    public  RecordOutputHandler getRecordOutputHandler() {
+        return (RecordOutputHandler)getRepository().getOutputHandler(org.ramadda.data.services.PointOutputHandler.class);
     }
 
 
