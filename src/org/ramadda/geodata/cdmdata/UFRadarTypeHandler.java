52cfc1bd11178a61555ad4da198261a7e803b494
ramadda-idv <ramadda-idv@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-05-09 16:15:18 +0000

    adding UF and Dorade format radar entry

---
+++ src/org/ramadda/geodata/cdmdata/UFRadarTypeHandler.java
@@ -0,0 +1,30 @@
+package org.ramadda.geodata.cdmdata;
+
+import org.ramadda.repository.Repository;
+import org.w3c.dom.Element;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: opendap
+ * Date: 5/4/13
+ * Time: 9:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UFRadarTypeHandler extends RadarTypeHandler {
+
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param entryNode _more_
+     *
+     * @throws Exception _more_
+     */
+    public UFRadarTypeHandler(Repository repository, Element entryNode)
+            throws Exception {
+        super(repository, entryNode);
+    }
+
+
+}
