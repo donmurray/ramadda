cffcbebba953e40e2466d6bf07613fbfccd1d040
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-06-12 12:23:00 +0000

    snapshto

--- src/org/ramadda/repository/type/Column.java
+++ src/org/ramadda/repository/type/Column.java
@@ -2086,7 +2086,7 @@ public class Column implements DataTypes, Constants {
                 //                                           rows, columns);
             } else {
                 widget = HtmlUtils.input(id, request.getString(id, ""),
-                                         "size=\"" + columns + "\"");
+                                         HtmlUtils.SIZE_50);
             }
         }
         formBuffer.append(typeHandler.formEntry(request, getLabel() + ":",
