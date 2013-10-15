1d4c42c6582e7b86702989fc1a755d78fa6f76c3
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-10-15 20:42:08 +0000

    snapshot of initial gml work

---
+++ src/org/ramadda/util/GmlUtil.java
@@ -0,0 +1,96 @@
+/*
+* Copyright 2008-2013 Geode Systems LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.util;
+
+
+/**
+ */
+public class GmlUtil {
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_GML = "http://www.opengis.net/gml";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_MS =
+        "http://mapserver.gis.umn.edu/mapserver";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_OGC = "http://www.opengis.net/ogc";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_XSI =
+        "http://www.w3.org/2001/XMLSchema-instance";
+
+    /** _more_          */
+    public static final String TAG_GML_BOUNDEDBY = "gml:boundedBy";
+
+    /** _more_          */
+    public static final String TAG_GML_BOX = "gml:Box";
+
+    /** _more_          */
+    public static final String TAG_GML_COORDINATES = "gml:coordinates";
+
+    /** _more_          */
+    public static final String TAG_GML_FEATUREMEMBER = "gml:featureMember";
+
+    /** _more_          */
+    public static final String TAG_MS_POLYGON = "ms:polygon";
+
+    /** _more_          */
+    public static final String TAG_MS_MSGEOMETRY = "ms:msGeometry";
+
+    /** _more_          */
+    public static final String TAG_GML_MULTIPOLYGON = "gml:MultiPolygon";
+
+    /** _more_          */
+    public static final String TAG_GML_POLYGONMEMBER = "gml:polygonMember";
+
+    /** _more_          */
+    public static final String TAG_GML_POLYGON = "gml:Polygon";
+
+    /** _more_          */
+    public static final String TAG_GML_OUTERBOUNDARYIS =
+        "gml:outerBoundaryIs";
+
+    /** _more_          */
+    public static final String TAG_GML_LINEARRING = "gml:LinearRing";
+
+    /** _more_          */
+    public static final String TAG_GML_INNERBOUNDARYIS =
+        "gml:innerBoundaryIs";
+
+    /** _more_          */
+    public static final String TAG_MS_OGC_FID = "ms:ogc_fid";
+
+    /** _more_          */
+    public static final String TAG_MS_NAME = "ms:name";
+
+    /** _more_          */
+    public static final String TAG_MS_ID = "ms:id";
+
+    /** _more_          */
+    public static final String ATTR_SRSNAME = "srsName";
+
+    /** _more_          */
+    public static final String ATTR_FID = "fid";
+
+
+}
