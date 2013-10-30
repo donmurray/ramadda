fe15a1f98ce4d0ed0ef597121c40a1a04bc55058
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-10-30 20:13:58 +0000

    new scp-s

--- bin/sfrelease.sh
+++ bin/sfrelease.sh
@@ -11,12 +11,10 @@ fi
 
 dest=/home/frs/project/r/ra/ramadda/ramadda${RAMADDA_VERSION}
 
-#Make the release
-#ant release
-
 #scp all of the plugins over to SF
+#scp dist/nlasplugin.zip ${sfuser},ramadda@frs.sourceforge.net:${dest}/plugins
 scp dist/otherplugins/* ${sfuser},ramadda@frs.sourceforge.net:${dest}/plugins
-
+#scp dist/pointtools.zip ${sfuser},ramadda@frs.sourceforge.net:${dest}
 
 #scp the top level build products to SF
 scp dist/repository.war dist/ramadda${RAMADDA_VERSION}.zip  dist/allplugins.jar dist/ramaddaclient.zip  ${sfuser},ramadda@frs.sourceforge.net:${dest}
