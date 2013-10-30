af899ae5aaca6038e80fb2025f50bbb8f3dcac5a
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-10-30 23:41:39 +0000

    new class

---
+++ src/org/ramadda/util/DatedObject.java
@@ -0,0 +1,95 @@
+/*
+* Copyright 2008-2013 Geode Systems LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.util;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+
+
+
+/**
+ * Provides a wrapper around an object and keeps track of a date.
+ *
+ * @param <ObjectType>
+ */
+public class DatedObject<ObjectType> {
+
+    /** _more_          */
+    private Date date;
+
+    /** _more_          */
+    private ObjectType object;
+
+
+    /**
+     * ctor
+     *
+     * @param date _more_
+     * @param object _more_
+     */
+    public DatedObject(Date date, ObjectType object) {
+        this.date   = date;
+        this.object = object;
+    }
+
+    /**
+     * Set the Date property.
+     *
+     * @param value The new value for Date
+     */
+    public void setDate(Date value) {
+        date = value;
+    }
+
+    /**
+     * Get the Date property.
+     *
+     * @return The Date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Set the Object property.
+     *
+     * @param value The new value for Object
+     */
+    public void setObject(ObjectType value) {
+        object = value;
+    }
+
+    /**
+     * Get the Object property.
+     *
+     * @return The Object
+     */
+    public ObjectType getObject() {
+        return object;
+    }
+
+
+
+}
