cd37dad9046a9fbd0093e502893e7f3461065973
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-10-31 10:31:39 +0000

    add pointtools to release

--- bin/sfrelease.sh
+++ bin/sfrelease.sh
@@ -12,12 +12,10 @@ fi
 dest=/home/frs/project/r/ra/ramadda/ramadda${RAMADDA_VERSION}
 
 #scp all of the plugins over to SF
-#scp dist/nlasplugin.zip ${sfuser},ramadda@frs.sourceforge.net:${dest}/plugins
-scp dist/otherplugins/* ${sfuser},ramadda@frs.sourceforge.net:${dest}/plugins
-#scp dist/pointtools.zip ${sfuser},ramadda@frs.sourceforge.net:${dest}
+scp ~/.ramadda/plugins/nlasplugin.jar dist/otherplugins/* ${sfuser},ramadda@frs.sourceforge.net:${dest}/plugins
 
 #scp the top level build products to SF
-scp dist/repository.war dist/ramadda${RAMADDA_VERSION}.zip  dist/allplugins.jar dist/ramaddaclient.zip  ${sfuser},ramadda@frs.sourceforge.net:${dest}
+scp dist/repository.war dist/ramadda${RAMADDA_VERSION}.zip  dist/allplugins.jar dist/ramaddaclient.zip  dist/pointtools.zip ${sfuser},ramadda@frs.sourceforge.net:${dest}
 
 
 
