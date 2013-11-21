db87790f3ab454d112fcb1c3f3cf66fd927a6230
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-11-21 12:33:16 +0000

    snapshot

---
+++ src/org/ramadda/data/services/ClimateIndexTypeHandler.java
@@ -0,0 +1,126 @@
+/*
+* Copyright 2008-2013 Geode Systems LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.data.services;
+
+
+import org.ramadda.data.point.PointFile;
+import org.ramadda.data.point.PointMetadataHarvester;
+import org.ramadda.data.point.text.MultiMonthFile;
+import org.ramadda.data.record.RecordFile;
+import org.ramadda.data.record.RecordFileFactory;
+import org.ramadda.data.record.RecordVisitorGroup;
+
+import org.ramadda.data.record.VisitInfo;
+import org.ramadda.data.services.PointEntry;
+
+import org.ramadda.data.services.RecordEntry;
+
+import org.ramadda.repository.*;
+import org.ramadda.repository.map.*;
+import org.ramadda.repository.metadata.*;
+import org.ramadda.repository.output.*;
+import org.ramadda.repository.type.*;
+import org.ramadda.util.FormInfo;
+import org.ramadda.util.HtmlUtils;
+import org.ramadda.util.Utils;
+import org.ramadda.util.grid.LatLonGrid;
+
+
+
+import org.w3c.dom.*;
+
+import ucar.unidata.util.Misc;
+
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
+
+/**
+ *
+ *
+ * @author Jeff McWhirter
+ * @version $Revision: 1.3 $
+ */
+public class ClimateIndexTypeHandler extends PointTypeHandler {
+
+
+    /**
+     * _more_
+     *
+     * @param repository ramadda
+     * @param node _more_
+     * @throws Exception On badness
+     */
+    public ClimateIndexTypeHandler(Repository repository, Element node)
+            throws Exception {
+        super(repository, node);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param entry _more_
+     *
+     * @throws Exception On badness
+     */
+    @Override
+    public void initializeNewEntry(Entry entry) throws Exception {
+        if (anySuperTypesOfThisType()) {
+            return;
+        }
+        //TODO: read the file, pull out the missing value, etc., then have the super init
+        super.initializeNewEntry(entry);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param recordEntry _more_
+     * @param metadata _more_
+     *
+     * @throws Exception _more_
+     */
+    protected void handleHarvestedMetadata(RecordEntry recordEntry,
+                                           PointMetadataHarvester metadata)
+            throws Exception {
+        super.handleHarvestedMetadata(recordEntry, metadata);
+        Entry          entry = recordEntry.getEntry();
+        MultiMonthFile mmf   = (MultiMonthFile) recordEntry.getRecordFile();
+        //Any further initialization?
+    }
+
+
+}
