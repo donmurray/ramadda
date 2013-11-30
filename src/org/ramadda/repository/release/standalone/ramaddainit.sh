01bfea3e02d0d12bf0d1f375f2c06eca04c92adf
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-11-30 12:00:30 +0000

    new init script. get rid of old one

---
+++ src/org/ramadda/repository/release/standalone/ramaddainit.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#
+RAMADDA_DIR=`dirname $0`
+
+BASE=$RAMADDA_DIR
+PID=$BASE/ramadda.pid
+LOG=$BASE/ramadda.log
+COMMAND="sh $RAMADDA_DIR/ramadda.sh"
+
+status() {
+    echo "RAMADDA Status"
+    if [ -f $PID ]
+    then
+        echo
+        echo "Pid file: $( cat $PID ) [$PID]"
+        echo
+        ps -ef | grep -v grep | grep $( cat $PID )
+    else
+        echo
+        echo "No Pid file"
+    fi
+}
+
+start() {
+    if [ -f $PID ]
+    then
+        echo
+        echo "RAMADDA already started. PID: [$( cat $PID )]"
+    else
+        echo "RAMADDA Start"
+        touch $PID
+        if nohup $COMMAND >>$LOG 2>&1 &
+        then echo $! >$PID
+             echo "Done."
+             echo "$(date '+%Y-%m-%d %X'): START" >>$LOG
+        else echo "Error... "
+             /bin/rm $PID
+        fi
+    fi
+}
+
+kill_cmd() {
+    SIGNAL=""; MSG="Killing RAMADDA"
+    while true
+    do
+        LIST=`ps -ef | grep -v grep | grep java | grep ramadda.jar |  awk '{print $2}'`
+        echo GOT
+        echo $LIST
+        if [ "$LIST" ]
+        then
+            echo; echo "$MSG $LIST" ; echo
+            echo $LIST | xargs kill $SIGNAL
+            sleep 2
+            SIGNAL="-9" ; MSG="Killing $SIGNAL"
+            if [ -f $PID ]
+            then
+                /bin/rm $PID
+            fi
+        else
+           echo; echo "All killed..." ; echo
+           break
+        fi
+    done
+}
+
+stop() {
+    echo "==== Stop"
+
+    if [ -f $PID ]
+    then
+        if kill $( cat $PID )
+        then echo "Done."
+             echo "$(date '+%Y-%m-%d %X'): STOP" >>$LOG
+        fi
+        /bin/rm $PID
+        kill_cmd
+    else
+        echo "No pid file. Already stopped?"
+    fi
+}
+
+case "$1" in
+    'start')
+            start
+            ;;
+    'stop')
+            stop
+            ;;
+    'restart')
+            stop ; echo "Sleeping..."; sleep 1 ;
+            start
+            ;;
+    'status')
+            status
+            ;;
+    *)
+            echo
+            echo "Usage: $0 { start | stop | restart | status }"
+            echo
+            exit 1
+            ;;
+esac
+
+exit 0
