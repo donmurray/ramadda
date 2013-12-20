c82b4c3970a3a622e841eb8c73f4f220a164fffb
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2013-12-20 21:43:23 +0000

    new js file

---
+++ src/org/ramadda/repository/htdocs/ramaddachart.js
@@ -0,0 +1,11 @@
+
+
+function RamaddaChart(id) {
+    this.data=
+    [["Site_Id","Latitude","Longitude","Elevation","Year","Julian_Day","Month","Day","Time","Temperature","Pressure","Wind_Speed","Wind_Direction","Relative_Humidity","Delta_T"],
+     ["BPT",-64.77,-64.06,8.0,2001.0,335.0,12.0,1.0,"0000",-1.1,null,null,null,null,null],
+     ["BPT",-64.77,-64.06,8.0,2001.0,335.0,12.0,1.0,"0010",-1.0,null,null,null,null,null],
+     ["BPT",-64.77,-64.06,8.0,2001.0,335.0,12.0,1.0,"0020",-1.1,null,null,null,null,null],
+     ["BPT",-64.77,-64.06,8.0,2001.0,335.0,12.0,1.0,"0030",-1.1,null,null,null,null,null],
+     ["BPT",-64.77,-64.06,8.0,2001.0,335.0,12.0,1.0,"0040",-1.0,null,null,null,null,null]];
+}
