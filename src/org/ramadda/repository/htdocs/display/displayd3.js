e4fda8ccdfb890a533410fd2e167ee75b8fa8a5c
oceandrivers <oceandrivers@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-02-27 21:36:08 +0000

    Modifications in the D3 line chart. Zoom implemented. Color bar addition. // TO DO: Add properties to the lineChart to define where to place the axis, the coloring function, etc. This will work for Profiles and even we can create cross sections for gliders tracks.

--- src/org/ramadda/repository/htdocs/display/displayd3.js
+++ src/org/ramadda/repository/htdocs/display/displayd3.js
@@ -2,8 +2,30 @@
 Copyright 2008-2014 Geode Systems LLC
 */
 
-// Testing color
+// Lets see if we create a support class for D3
+// This will be the default but we can add more colorscales
 
+
+					
+function getColorFromColorBar(value,range){
+	
+	console.log(value);
+	console.log(range);
+	
+	var colors = ["#00008B","#0000CD","#0000FF","#00FFFF","#7CFC00","#FFFF00","#FFA500","#FF4500","#FF0000","#8B0000"];
+	
+	var colorScale = d3.scale.linear()
+		.domain([0, colors.length-1])
+		.range(range);
+	
+	var colorScaler = d3.scale.linear()
+						.range(colors)
+						.domain(d3.range(0,colors.length).map(colorScale));
+	
+	color = colorScaler(value);
+	console.log(color);
+	return color;
+}
 
 function RamaddaD3Display(displayManager, id, properties) {
     var ID_SVG = "svg";
@@ -86,22 +108,13 @@ function RamaddaD3Display(displayManager, id, properties) {
 					
 					var extentZ = d3.extent(records, function (d) { return records;} )
 					
-					var colors = ["#00008B","#0000CD","#0000FF","#00FFFF","#7CFC00","#FFFF00","#FFA500","#FF4500","#FF0000","#8B0000"];
-
-					var colorScale = d3.scale.linear()
-						.domain([0, colors.length-1])
-						.range(range);
-						
-					var colorScaler = d3.scale.linear()
-										.range(colors)
-										.domain(d3.range(0,colors.length).map(colorScale));
-
                     var rects = svg.
                         append("svg:svg").
                         attr("width", width).
                         attr("height", height).
                         attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-                    //Draw the rects
+                    
+					//Draw the rects
                     rects.selectAll("rect").
                         data(records).
                         enter().
@@ -110,15 +123,16 @@ function RamaddaD3Display(displayManager, id, properties) {
                                 return xScale(index); 
                             }).
                         attr("y", function(record) { 
-                                return  yScale(record.getElevation())+barHeight/2; 
+								return  yScale(record.getElevation())+barHeight/2; 
                             }).
                         attr("height", barHeight).
                         attr("width", barWidth).
                         attr("fill", function(record) {
+						
                                 var value = record.getValue(dataIndex);
                                 //var scale = (value-range[0])/(range[1]-range[0]);
                                 //var v = parseInt(scale*255);
-                                return colorScaler(value);
+                                return getColorFromColorBar(value,range);
                             });
                 }
 
@@ -148,10 +162,11 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
     $.extend(this, {
             initDisplay: function() {
                 this.initUI();
+								
                 this.setTitle("D3 LineChart");
 				
 				var height = this.getProperty("height",300);
-				var margin = {top: 20, left: 50, bottom: 20, right: 20};
+				var margin = {top: 20, right: 50, bottom: 30, left: 50};
 				
                 var html = HtmlUtil.div([ATTR_ID, this.getDomId(ID_SVG),"style","height:" + height +"px;"],"");
                 this.setContents(html);
@@ -166,9 +181,7 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
 
 				this.y = d3.scale.linear()
 					.range([displayHeight, 0]);
-
-			    
-							
+			
 				this.xAxis = d3.svg.axis()
 					.scale(this.x)
 					.orient("bottom");
@@ -177,31 +190,49 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
 				this.yAxis = d3.svg.axis()
 					.scale(this.y)
 					.orient("left");
-
-				this.svg = d3.select("#" + this.getDomId(ID_SVG)).append("svg")
-							.attr("width", displayWidth + margin.left + margin.right)
-							.attr("height", displayHeight + margin.top + margin.bottom)
-						  .append("g")
-							.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-				 
-				
-				
+					
 				// To solve the problem with the classess within the class
 				var myThis = this;
-				// This will be the event handler. In this case the zoom will be only inside the graph area nor the axis
-				this.svg.append("svg:rect")
-					.attr("width", displayWidth)
-					.attr("height", displayHeight)
-					.attr(ATTR_ID,"rect_"+this.getDomId(ID_SVG))
-					.call(d3.behavior.zoom().on("zoom", function(){myThis.zoomBehaviour()}))
-					.on("click", function(){myThis.click(event)});
-					
+				
+				
+				var zoom = d3.behavior.zoom()
+							.on("zoom", function() {myThis.zoomBehaviour()});
+				
+				this.zoom = zoom;
+				
+				this.svg = d3.select("#" + this.getDomId(ID_SVG)).append("svg")
+								.attr("width", displayWidth + margin.left + margin.right)
+								.attr("height", displayHeight + margin.top + margin.bottom)
+								.call(zoom)
+								.on("click", function(){myThis.click(event)})
+								.on("dblclick", function(){myThis.dbclick(event)})
+						    .append("g")
+								.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+				
+				// Add the axis
+				this.svg.append("g")
+				  .attr(ATTR_CLASS, "x axis")
+				  .attr("transform", "translate(0," + displayHeight + ")")
+				  .attr("fill","none")
+				  .attr("stroke","#555555")
+				  .attr("shape-rendering","crispEdges")
+				  .call(this.xAxis);
+				  
+
+				this.svg.append("g")
+				  .attr(ATTR_CLASS, "y axis")
+				  .call(this.yAxis)
+				  .attr("fill","none")
+				  .attr("stroke","#555555")
+				  .attr("shape-rendering","crispEdges");
+				
+				
 				this.displayWidth=displayWidth;
 				this.displayHeight=displayHeight;
 				this.color = d3.scale.category10();
 				
                 this.updateUI();
+				
             },
             needsData: function() {return true;},
             initDialog: function() {
@@ -216,12 +247,9 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
             fieldSelectionChanged: function() {
                 this.updateUI();
             },
-            updateUI: function() {
+            updateUI: function(onlyZoom) {
 			
-				// To be used inside a function we can use this.x inside them so we extract as variables. 
-				var x = this.x;
-				var y = this.y;
-				var color = this.color;
+				test = this;
 				
 				var selectedFields = this.getSelectedFields();
                 if(selectedFields.length==0) {
@@ -236,52 +264,76 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
                     return;
                 }
 				
-				
 				var fields = pointData.getNumericFields();
                 var records = pointData.getRecords();
                 var ranges =  RecordUtil.getRanges(fields,records);
                 var elevationRange =  RecordUtil.getElevationRange(fields,records);
                 var offset = (elevationRange[1] - elevationRange[0])*0.05;
-
-				this.x.domain(d3.extent(records, function(d) { return new Date(d.getDate()); }));
-				// the y domain depends on the first selected element I have to think about it.
-				this.y.domain(d3.extent(records, function(d) { return d.getData()[selectedFields[0].getIndex()]; }));
 				
-				this.svg.append("g")
-				  .attr(ATTR_CLASS, "x axis")
-				  .attr("transform", "translate(0," + this.displayHeight + ")")
-				  .attr("fill","none")
-				  .attr("stroke","#555555")
-				  .attr("shape-rendering","crispEdges")
-				  .call(this.xAxis);
-				  
-
-				this.svg.append("g")
-				  .attr(ATTR_CLASS, "y axis")
-				  .call(this.yAxis)
-				  .attr("fill","none")
-				  .attr("stroke","#555555")
-				  .attr("shape-rendering","crispEdges");
-					
+				// To be used inside a function we can use this.x inside them so we extract as variables. 
+				var x = this.x;
+				var y = this.y;
+				var color = this.color;
+				
+				if(onlyZoom){
+					this.zoom.x(this.x);
+					this.zoom.y(this.y);
+				}else{
+					// Update axis for the zoom and other changes
+					this.x.domain(d3.extent(records, function(d) { return new Date(d.getDate()); }));
+					// the y domain depends on the first selected element I have to think about it.
+					this.y.domain(d3.extent(records, function(d) { return d.getData()[selectedFields[0].getIndex()]; }));
+					this.zoom.x(this.x);
+					this.zoom.y(this.y);
+				}
+				
+				this.svg.selectAll(".y.axis")
+					.call(this.yAxis);
+			
+				this.svg.selectAll(".x.axis")
+					.call(this.xAxis);
+				
+				// Remove previous lines
+				this.svg.selectAll(".line").remove();
+				this.svg.selectAll(".legendElement").remove();
+				
+				
+				
 				for(var fieldIdx=0;fieldIdx<selectedFields.length;fieldIdx++) {
-                    /*var dataIndex = selectedFields[fieldIdx].getIndex();
-                    var range = ranges[dataIndex];
-					*/
+                    var dataIndex=selectedFields[fieldIdx].getIndex()
+					var range = ranges[dataIndex];
+					
 					
 					// Plot line for the values
 					var line = d3.svg.line()
 						.x(function(d) { return x(new Date(d.getDate())); })
-						.y(function(d) { return y(d.getData()[selectedFields[fieldIdx].getIndex()]); });
+						.y(function(d) { return y(d.getData()[dataIndex]); });
+					
+					// For the color
+					var extentZ = d3.extent(records, function(d) { return y(d.getData()[dataIndex]); })
+					var colors = ["#00008B","#0000CD","#0000FF","#00FFFF","#7CFC00","#FFFF00","#FFA500","#FF4500","#FF0000","#8B0000"];
+
+					var colorSpacing = 100 / ( colors.length - 1 );
+
+					var colorBarScale = d3.scale.linear()
+							.range([ 0, this.displayHeight])
+							.domain(extentZ);
+
+					var colorBarAxisY = d3.svg.axis()
+							.scale( colorBarScale  )
+							.ticks( 10 )
+							.tickSubdivide( true )
+							.orient("right");
+
 					
 					this.svg.append("path")
 					  .datum(records)
 					  .attr(ATTR_CLASS, "line")
 					  .attr("d", line)
-					  .attr("fill","none")
-					  .attr("stroke",function(d){return color(fieldIdx);})
+					  .attr("fill", "none")
+					  .attr("stroke", "url(#colorBarGradient)")
 					  .attr("stroke-width","0.5px");
-					
-					
+                      
 					// Plot moving average Line
 					var movingAverageLine = d3.svg.line()
 							.x(function(d) { return x(new Date(d.getDate())); })
@@ -301,11 +353,14 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
 					  .attr("fill","none")
 					  .attr("stroke",function(d){return color(fieldIdx);})
 					  .attr("stroke-width","1.5px")
+					  .attr("viewBox", "50 50 100 100 ")
 					  .style("stroke-dasharray", ("3, 3"));
+					  
 					  
 					
 					// Legend element Maybe create a function or see how we implement the legend
 				    this.svg.append("svg:rect")
+					   .attr("class","legendElement")
 					   .attr("x", this.displayWidth-100)
 					   .attr("y", (50+50*fieldIdx))
 					   .attr("stroke", function(d){return color(fieldIdx);})
@@ -313,6 +368,7 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
 					   .attr("width", 40);
 					   
 					this.svg.append("svg:text")
+						    .attr("class","legendElement")
 						   .attr("x", this.displayWidth-100+40+10) // position+color rect+padding
 						   .attr("y", (55+55*fieldIdx))
 						   .attr("stroke", function(d){return color(fieldIdx);})
@@ -320,23 +376,44 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
 						   .text(selectedFields[fieldIdx].getLabel());
 				}
 
+				// Plot Color 
+				var colorBar = this.svg.append("g")
+						.attr({
+							"id"        : "colorBarG",
+							"transform" : "translate(" + (this.displayWidth+40) + ",0)"
+						});
+
+					colorBar.append("g")
+						.append("defs")
+						.append("linearGradient")
+						.attr({
+							id : "colorBarGradient",
+							x1 : "0%",
+							y1 : "100%",
+							x2 : "0%",
+							y2 : "0%"
+						})
+						.selectAll("stop")
+						.data(colors)
+						.enter()
+						.append("stop")
+						.attr({
+							"offset": function(d,i){return colorSpacing * (i) + "%"},
+							"stop-color":function(d,i){return colors[i]},
+							"stop-opacity":1
+						});
 
             },
 			zoomBehaviour: function(){
 				// I think we will have to do this nightmare...
-				console.log(d3.event.translate);
-				console.log(d3.event.scale);
 				
 				// This will zoom the entire graph I will keep it here for other visualizations
-				this.svg.attr("transform",
+				/* this.svg.attr("transform",
 					  "translate(" + d3.event.translate + ")"
-					  + " scale(" + d3.event.scale + ")");
+					  + " scale(" + d3.event.scale + ")"); */
 				
-				testing=this;
-				/*this.svg.select("g.x.axis").call(this.xAxis);
-			    this.svg.select("g.y.axis").call(this.yAxis);*/
-				
-				this.updateUI();
+				// Call redraw with only zoom don't update extent of the data.
+				this.updateUI(true);
 				
 			},
             //this gets called when an event source has selected a record
@@ -344,8 +421,13 @@ function RamaddaD3LineChartDisplay(displayManager, id, properties) {
                 //this.setContents(args.html);
             },
             click: function(event) {
-                console.log("Clicked");
+				// TO DO
 				console.log(event);
+            },
+			dbclick:function(event) {
+				// Unzoom
+				this.zoom
+				this.updateUI();
             },
 			getSVG: function() {
 				return this.svg;
