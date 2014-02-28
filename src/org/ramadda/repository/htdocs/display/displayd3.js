eab53791e37b55c6bed5b605d6ccc3f474f67dbf
oceandrivers <oceandrivers@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-02-28 18:15:09 +0000

    Profile Chart for CNV (Tested for CNV files only the getElevation is not working and I added manually, then you plot temperature variable and remove the depth) Cross section for gliders (TODO: Check the color because is not related to the data extent yet) Added properties for the D3 charts to define axis/colors/default fields probably this should go up to the ramaddaDisplay level.
            properties["graph"]={};
    	properties["graph"]["title"]="Glider cross section";
    	properties["graph"]["axis"]={};
    	properties["graph"]["axis"]["y"]={}
    	properties["graph"]["axis"]["y"]["type"]="elevation";
    	properties["graph"]["axis"]["y"]["fieldname"]="depth";
    	properties["graph"]["axis"]["y"]["reverse"]="true";
    	properties["graph"]["axis"]["x"]={}
    	properties["graph"]["axis"]["x"]["type"]="time";
    	properties["graph"]["axis"]["x"]["fieldname"]="time";
    	properties["graph"]["axis"]["z"]="selectedField";
    	properties["graph"]["extraLine"]="none";

--- src/org/ramadda/repository/htdocs/display/displayd3.js
+++ src/org/ramadda/repository/htdocs/display/displayd3.js
@@ -3,13 +3,49 @@ Copyright 2008-2014 Geode Systems LLC
 */
 
 // Lets see if we create a support class for D3
-// This will be the default but we can add more colorscales
+function getAxis(axisType,range){
+	
+	var axis;
+	if ( axisType == "time"){
+		axis = d3.time.scale().range(range);
+	}else{
+		axis = d3.scale.linear().range(range);
+	}
+	return axis;
+}
 
-	
+function getDataValue(axis,value,index){
+
+	var data;
+	if(axis.fieldIdx>=0){
+		data = value.getData()[axis.fieldIdx];
+	}else{	
+		switch(axis.type){
+			case "time": 
+				data = new Date(value.getDate());
+				break
+			case "elevation":
+				console.log(value.getElevation());
+				data = value.getElevation();
+					break
+			case "latitude":
+				data = value.getLatitude();
+			case "longitude":
+				data = value.getLongitude();
+			default:
+				data = value.getData()[index];
+		}
+	}		
+
+    if(axis.reverse=="true"){
+		return -1*data;
+	}else{
+		return data;
+	}
+}
+
+// This will be the default but we can add more colorscales
 function getColorFromColorBar(value,range){
-	
-	console.log(value);
-	console.log(range);
 	
 	var colors = ["#00008B","#0000CD","#0000FF","#00FFFF","#7CFC00","#FFFF00","#FFA500","#FF4500","#FF0000","#8B0000"];
 	
@@ -25,19 +61,52 @@ function getColorFromColorBar(value,range){
 	return color;
 }
 
+// This is for the path lines the previous function for generic ones. 
+function addColorBar(svg,colors,colorSpacing){
+	var colorBar = svg.append("g")
+					.attr({
+						"id"        : "colorBarG",
+						"transform" : "translate(" + (this.displayWidth-40) + ",0)"
+					});
+
+	colorBar.append("g")
+		.append("defs")
+		.append("linearGradient")
+		.attr({
+			id : "colorBarGradient",
+			x1 : "0%",
+			y1 : "100%",
+			x2 : "0%",
+			y2 : "0%"
+		})
+		.selectAll("stop")
+		.data(colors)
+		.enter()
+		.append("stop")
+		.attr({
+			"offset": function(d,i){return colorSpacing * (i) + "%"},
+			"stop-color":function(d,i){return colors[i]},
+			"stop-opacity":1
+		});
+	
+	return colorBar;
+}
+
+
 function RamaddaD3Display(displayManager, id, properties) {
     var ID_SVG = "svg";
 	var SUPER; 
-    
+    // To get it to the console
+	testProperties = properties;
+	
 	RamaddaUtil.inherit(this, SUPER = new RamaddaDisplay(displayManager, id, "d3", properties));
     addRamaddaDisplay(this);
-    console.log(properties);
 				
 	$.extend(this, {
             initDisplay: function() {
                 this.initUI();
 								
-                this.setTitle("D3 LineChart");
+                this.setTitle(properties.graph.title);
 				
 				var height = this.getProperty("height",300);
 				var margin = {top: 20, right: 50, bottom: 30, left: 50};
@@ -56,56 +125,40 @@ function RamaddaD3Display(displayManager, id, properties) {
 							.on("zoom", function() {myThis.zoomBehaviour()});
 				this.zoom = zoom;
 				
-				var drag = d3.behavior.drag()
-							.on("drag",function() {myThis.zoomBehaviour()});
-				this.drag = drag;
 				
 				this.svg = d3.select("#" + this.getDomId(ID_SVG)).append("svg")
 								.attr("width", displayWidth + margin.left + margin.right)
 								.attr("height", displayHeight + margin.top + margin.bottom)
 								.attr("class","D3graph")
 								.call(zoom)
-								.call(drag)
 								.on("click", function(){myThis.click(d3.event)})
 								.on("dblclick", function(){myThis.dbclick(d3.event)})
 						    .append("g")
 								.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 				
-				
-				
 				// Define the Axis
 				// 100 pixels for the legend... lets see if we keep it
-				this.x = d3.time.scale()
-					.range([0, displayWidth-100]);
-
-				this.y = d3.scale.linear()
-					.range([displayHeight, 0]);
+				this.x = getAxis(properties.graph.axis.x.type,[0,displayWidth-100]);
+				this.y = getAxis(properties.graph.axis.y.type,[displayHeight,0]);
 			
 				this.xAxis = d3.svg.axis()
 					.scale(this.x)
 					.orient("bottom");
-					//.ticks(5);
 
+				
 				this.yAxis = d3.svg.axis()
 					.scale(this.y)
 					.orient("left");
-				
+			
 				// Add Axis to the plot
 				this.svg.append("g")
 				  .attr(ATTR_CLASS, "x axis")
 				  .attr("transform", "translate(0," + displayHeight + ")")
-				  .attr("fill","none")
-				  .attr("stroke","#555555")
-				  .attr("shape-rendering","crispEdges")
 				  .call(this.xAxis);
 				  
-
 				this.svg.append("g")
 				  .attr(ATTR_CLASS, "y axis")
-				  .call(this.yAxis)
-				  .attr("fill","none")
-				  .attr("stroke","#555555")
-				  .attr("shape-rendering","crispEdges");
+				  .call(this.yAxis);
 				
 				
 				// Color Bar
@@ -113,35 +166,12 @@ function RamaddaD3Display(displayManager, id, properties) {
 
 				var colorSpacing = 100 / ( colors.length - 1 );
 				
-				var colorBar = this.svg.append("g")
-						.attr({
-							"id"        : "colorBarG",
-							"transform" : "translate(" + (this.displayWidth+40) + ",0)"
-						});
-
-					colorBar.append("g")
-						.append("defs")
-						.append("linearGradient")
-						.attr({
-							id : "colorBarGradient",
-							x1 : "0%",
-							y1 : "100%",
-							x2 : "0%",
-							y2 : "0%"
-						})
-						.selectAll("stop")
-						.data(colors)
-						.enter()
-						.append("stop")
-						.attr({
-							"offset": function(d,i){return colorSpacing * (i) + "%"},
-							"stop-color":function(d,i){return colors[i]},
-							"stop-opacity":1
-						});
+				var colorBar = addColorBar(this.svg,colors,colorSpacing);
 				
 				this.displayWidth=displayWidth;
 				this.displayHeight=displayHeight;
 				this.color = d3.scale.category10();
+				
 				
                 this.updateUI();
 				
@@ -183,20 +213,21 @@ function RamaddaD3Display(displayManager, id, properties) {
                 var elevationRange =  RecordUtil.getElevationRange(fields,records);
                 var offset = (elevationRange[1] - elevationRange[0])*0.05;
 				
-				
 				// To be used inside a function we can use this.x inside them so we extract as variables. 
 				var x = this.x;
 				var y = this.y;
 				var color = this.color;
 				
+				var axis = properties.graph.axis;
 				if(onlyZoom){
 					this.zoom.x(this.x);
 					this.zoom.y(this.y);
 				}else{
+					
 					// Update axis for the zoom and other changes
-					this.x.domain(d3.extent(records, function(d) { return new Date(d.getDate()); }));
+					this.x.domain(d3.extent(records, function(d) { return getDataValue(axis.x,d,selectedFields[0].getIndex()); }));
 					// the y domain depends on the first selected element I have to think about it.
-					this.y.domain(d3.extent(records, function(d) { return d.getData()[selectedFields[0].getIndex()]; }));
+					this.y.domain(d3.extent(records, function(d) { return getDataValue(axis.y,d,selectedFields[0].getIndex()); }));
 					
 					this.zoom.x(this.x);
 					this.zoom.y(this.y);
@@ -220,41 +251,47 @@ function RamaddaD3Display(displayManager, id, properties) {
 					
 					// Plot line for the values
 					var line = d3.svg.line()
-						.x(function(d) { return x(new Date(d.getDate())); })
-						.y(function(d) { return y(d.getData()[dataIndex]); });
+						.x(function(d) { return x( getDataValue(axis.x,d,selectedFields[fieldIdx].getIndex())); })
+						.y(function(d) { return y( getDataValue(axis.y,d,selectedFields[fieldIdx].getIndex())); });
 					
 					myThis=this;
-					this.svg.append("path")
-								  .datum(records)
-								  .attr("class", "line")
-								  .attr("d", line)
-								  .on("mousemove", function(){myThis.mouseover(d3.event)})
-								  .attr("fill", "none")
-								  .attr("stroke", "url(#colorBarGradient)")
-								  .attr("stroke-width","0.5px");
-					 
-					// Plot moving average Line
-					var movingAverageLine = d3.svg.line()
-							.x(function(d) { return x(new Date(d.getDate())); })
-							.y(function(d,i) {
-								if (i == 0) {
-									return _movingSum = d.getData()[selectedFields[fieldIdx].getIndex()];
-								} else {
-									_movingSum += d.getData()[selectedFields[fieldIdx].getIndex()];
-								}
-								return y(_movingSum / i);
-							})
-							.interpolate("basis");
+					displayLine = this.svg.append("path")
+									  .datum(records)
+									  .attr("class", "line")
+									  .attr("d", line)
+									  .on("mousemove", function(){myThis.mouseover(d3.event)})
+									  .attr("fill", "none")
+									  .attr("stroke",function(d){return color(fieldIdx);})
+									  .attr("stroke-width","0.5px");
+
+					if(properties.graph.axis.z=="selectedField"){
+						displayLine.attr("stroke", "url(#colorBarGradient)")
+									  
+					}
 					
-					this.svg.append("path")
-							  .attr("class", "line")
-							  .attr("d", movingAverageLine(records))
-							  .attr("fill","none")
-							  .attr("stroke",function(d){return color(fieldIdx);})
-							  .attr("stroke-width","1.5px")
-							  .attr("viewBox", "50 50 100 100 ")
-							  .style("stroke-dasharray", ("3, 3"));
-					  
+					if(properties.graph.extraLine=="movingAverage"){
+						// Plot moving average Line
+						var movingAverageLine = d3.svg.line()
+								.x(function(d) { return x(getDataValue(axis.x,d,selectedFields[fieldIdx].getIndex())); })
+								.y(function(d,i) {
+									if (i == 0) {
+										return _movingSum = getDataValue(axis.y,d,selectedFields[fieldIdx].getIndex());
+									} else {
+										_movingSum +=  getDataValue(axis.y,d,selectedFields[fieldIdx].getIndex());
+									}
+									return y(_movingSum / i);
+								})
+								.interpolate("basis");
+						
+						this.svg.append("path")
+								  .attr("class", "line")
+								  .attr("d", movingAverageLine(records))
+								  .attr("fill","none")
+								  .attr("stroke",function(d){return color(fieldIdx);})
+								  .attr("stroke-width","1.5px")
+								  .attr("viewBox", "50 50 100 100 ")
+								  .style("stroke-dasharray", ("3, 3"));
+					}
 					  
 					
 					// Legend element Maybe create a function or see how we implement the legend
@@ -289,6 +326,7 @@ function RamaddaD3Display(displayManager, id, properties) {
 			mouseover: function() {
 				// TO DO
 				testing=d3.event;
+				console.log("mouseover");
 				
             },
             click: function(event) {
@@ -309,9 +347,18 @@ function RamaddaD3Display(displayManager, id, properties) {
 
 function RamaddaD3LineChartDisplay(displayManager, id, properties) {
 
-	properties["axis"]={};
-	properties["axis"]["y"]="value";
-	properties["axis"]["x"]="time";
+	properties["graph"]={};
+	properties["graph"]["title"]="Line Chart";
+	properties["graph"]["axis"]={};
+	properties["graph"]["axis"]["y"]={}
+	properties["graph"]["axis"]["y"]["type"]="value";
+	properties["graph"]["axis"]["y"]["fieldname"]="selectedField";
+	properties["graph"]["axis"]["x"]={}
+	properties["graph"]["axis"]["x"]["type"]="time";
+	properties["graph"]["axis"]["x"]["fieldname"]="time";
+	properties["graph"]["axis"]["z"]="none";
+	properties["graph"]["extraLine"]="movingAverage";
+	
 	return new RamaddaD3Display(displayManager,id,properties);
 	
 }
@@ -320,15 +367,43 @@ addGlobalDisplayType({type: "D3LineChart", label:"D3 LineChart"});
 
 function RamaddaProfileDisplay(displayManager, id, properties) {
 	
-	console.log(properties);
+	properties["graph"]={};
+	properties["graph"]["title"]="Profile chart";
+	properties["graph"]["axis"]={};
+	properties["graph"]["axis"]["y"]={}
+	properties["graph"]["axis"]["y"]["type"]="elevation";
+	properties["graph"]["axis"]["y"]["fieldname"]="depth";
+	properties["graph"]["axis"]["y"]["fieldIdx"]=3;
+	properties["graph"]["axis"]["y"]["reverse"]="true";
+	properties["graph"]["axis"]["x"]={}
+	properties["graph"]["axis"]["x"]["type"]="value";
+	properties["graph"]["axis"]["x"]["fieldname"]="value";
+	properties["graph"]["axis"]["z"]="none";
+	properties["graph"]["extraLine"]="none";
 	
-	properties["axis"]={};
-	properties["axis"]["y"]="depth";
-	properties["axis"]["x"]="value";
 	return new RamaddaD3Display(displayManager,id,properties);
 
 }
 
 addGlobalDisplayType({type: "Profile", label:"Profile"});
 
+function RamaddaGliderCrossSectionDisplay(displayManager, id, properties) {
+	
+	properties["graph"]={};
+	properties["graph"]["title"]="Glider cross section";
+	properties["graph"]["axis"]={};
+	properties["graph"]["axis"]["y"]={}
+	properties["graph"]["axis"]["y"]["type"]="elevation";
+	properties["graph"]["axis"]["y"]["fieldname"]="depth";
+	properties["graph"]["axis"]["y"]["reverse"]="true";
+	properties["graph"]["axis"]["x"]={}
+	properties["graph"]["axis"]["x"]["type"]="time";
+	properties["graph"]["axis"]["x"]["fieldname"]="time";
+	properties["graph"]["axis"]["z"]="selectedField";
+	properties["graph"]["extraLine"]="none";
 
+	return new RamaddaD3Display(displayManager,id,properties);
+
+}
+
+addGlobalDisplayType({type: "GliderCrossSection", label:"Glider cross section"});
