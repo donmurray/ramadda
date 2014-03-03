54d2ab7028ff962f92dc3164346b26dddd0b7218
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-03-03 11:05:19 +0000

    New data strucuture to hold map regions

---
+++ src/org/ramadda/util/MapRegion.java
@@ -0,0 +1,180 @@
+/*
+* Copyright 2008-2014 Geode Systems LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.util;
+
+
+
+/**
+ */
+public class MapRegion {
+
+    /** _more_          */
+    private String id;
+
+
+    /** _more_ */
+    private String name;
+
+    /** _more_ */
+    private double north;
+
+    /** _more_ */
+    private double west;
+
+    /** _more_ */
+    private double south;
+
+    /** _more_ */
+    private double east;
+
+    /**
+     * _more_
+     *
+     *
+     * @param id _more_
+     * @param name _more_
+     * @param north _more_
+     * @param west _more_
+     * @param south _more_
+     * @param east _more_
+     */
+    public MapRegion(String id, String name, double north, double west,
+                     double south, double east) {
+        this.id    = id;
+        this.name  = name;
+        this.north = north;
+        this.west  = west;
+        this.south = south;
+        this.east  = east;
+    }
+
+
+
+    /**
+     * Set the North property.
+     *
+     * @param value The new value for North
+     */
+    public void setNorth(double value) {
+        north = value;
+    }
+
+    /**
+     * Get the North property.
+     *
+     * @return The North
+     */
+    public double getNorth() {
+        return north;
+    }
+
+    /**
+     * Set the West property.
+     *
+     * @param value The new value for West
+     */
+    public void setWest(double value) {
+        west = value;
+    }
+
+    /**
+     * Get the West property.
+     *
+     * @return The West
+     */
+    public double getWest() {
+        return west;
+    }
+
+    /**
+     * Set the South property.
+     *
+     * @param value The new value for South
+     */
+    public void setSouth(double value) {
+        south = value;
+    }
+
+    /**
+     * Get the South property.
+     *
+     * @return The South
+     */
+    public double getSouth() {
+        return south;
+    }
+
+    /**
+     * Set the East property.
+     *
+     * @param value The new value for East
+     */
+    public void setEast(double value) {
+        east = value;
+    }
+
+    /**
+     * Get the East property.
+     *
+     * @return The East
+     */
+    public double getEast() {
+        return east;
+    }
+
+    /**
+     * Set the Name property.
+     *
+     * @param value The new value for Name
+     */
+    public void setName(String value) {
+        name = value;
+    }
+
+    /**
+     * Get the Name property.
+     *
+     * @return The Name
+     */
+    public String getName() {
+        return name;
+    }
+
+
+    /**
+     * Set the Id property.
+     *
+     * @param value The new value for Id
+     */
+    public void setId(String value) {
+        id = value;
+    }
+
+    /**
+     * Get the Id property.
+     *
+     * @return The Id
+     */
+    public String getId() {
+        return id;
+    }
+
+}
