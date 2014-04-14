9ada4ae77366a51dd7d66928c0c04382685ffe0c
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-04-14 16:36:56 +0000

    bump up the HTDOCS path version

--- src/org/ramadda/repository/RepositoryUtil.java
+++ src/org/ramadda/repository/RepositoryUtil.java
@@ -56,7 +56,7 @@ public class RepositoryUtil {
     //that gets replaced with  this. Repository checks incoming paths and strips this off
 
     /** _more_ */
-    public static final String HTDOCS_VERSION = "htdocs_v1_6a3";
+    public static final String HTDOCS_VERSION = "htdocs_v1_6b";
 
     /** _more_ */
     public static final String HTDOCS_VERSION_SLASH = "/" + HTDOCS_VERSION;
