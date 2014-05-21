06cebfe96cd88e33593c53bff7e9bc2f6c663d99
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-05-21 17:12:51 +0000

    change the htdocs version

--- src/org/ramadda/repository/RepositoryUtil.java
+++ src/org/ramadda/repository/RepositoryUtil.java
@@ -56,7 +56,7 @@ public class RepositoryUtil {
     //that gets replaced with  this. Repository checks incoming paths and strips this off
 
     /** _more_ */
-    public static final String HTDOCS_VERSION = "htdocs_v1_6b";
+    public static final String HTDOCS_VERSION = "htdocs_v1_7a";
 
     /** _more_ */
     public static final String HTDOCS_VERSION_SLASH = "/" + HTDOCS_VERSION;
