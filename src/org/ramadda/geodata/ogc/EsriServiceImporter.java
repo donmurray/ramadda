bf8873f7afb3dec1627b56104b6ff9431ba77495
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-07-31 20:07:39 +0000

    New entry types. Make the import be asynchronous

--- src/org/ramadda/geodata/ogc/EsriServiceImporter.java
+++ src/org/ramadda/geodata/ogc/EsriServiceImporter.java
@@ -26,6 +26,7 @@ import org.json.*;
 import org.ramadda.repository.*;
 import org.ramadda.repository.metadata.*;
 import org.ramadda.repository.metadata.Metadata;
+import org.ramadda.util.HtmlUtils;
 import org.ramadda.util.Utils;
 
 import ucar.unidata.util.IOUtil;
@@ -48,14 +49,28 @@ import java.util.List;
  */
 public class EsriServiceImporter extends ImportHandler {
 
+    /** _more_ */
     public static final String TAG_SERVICES = "services";
+
+    /** _more_ */
     public static final String TAG_FOLDERS = "folders";
+
+    /** _more_ */
     public static final String TAG_URL = "url";
 
+    /** _more_ */
     public static final String TAG_DESCRIPTION = "description";
+
+    /** _more_ */
     public static final String TAG_SERVICEDESCRIPTION = "serviceDescription";
-    public static final String TAG_FULLEXTENT =         "fullExtent";
+
+    /** _more_ */
+    public static final String TAG_FULLEXTENT = "fullExtent";
+
+    /** _more_ */
     public static final String TAG_SPATIALREFERENCE = "spatialReference";
+
+    /** _more_ */
     public static final String TAG_WKID = "wkid";
 
 
@@ -93,44 +108,78 @@ public class EsriServiceImporter extends ImportHandler {
      *
      * @param request _more_
      * @param repository _more_
-     * @param uploadedFile _more_
-     * @param url _more_
+     * @param theUrl _more_
      * @param parentEntry _more_
-     *
      * @return _more_
      *
      * @throws Exception _more_
      */
     @Override
-    public Result handleUrlRequest(Request request, Repository repository,
-                                   String url, Entry parentEntry)
+    public Result handleUrlRequest(final Request request,
+                                   Repository repository,
+                                   final String theUrl,
+                                   final Entry parentEntry)
             throws Exception {
         if ( !request.getString(ARG_IMPORT_TYPE, "").equals(TYPE_ESRI)) {
             return null;
         }
-        StringBuffer sb      = new StringBuffer();
-        List<Entry>  entries = new ArrayList<Entry>();
 
-        String[] toks =url.split("\\?");
-        url  = toks[0];
-        if(url.endsWith("/")) url = url.substring(0, url.length()-1);
+        ActionManager.Action action = new ActionManager.Action() {
+            public void run(Object actionId) throws Exception {
+                String       url     = theUrl;
+                StringBuffer sb      = new StringBuffer();
+                List<Entry>  entries = new ArrayList<Entry>();
+                String[]     toks    = url.split("\\?");
+                url = toks[0];
+                if (url.endsWith("/")) {
+                    url = url.substring(0, url.length() - 1);
+                }
 
-        processServiceList(request, parentEntry, entries, url,url);
+                processServiceList(request, parentEntry, actionId, entries,
+                                   url, url);
 
-        for (Entry entry : entries) {
-            entry.setUser(request.getUser());
-        }
-        getEntryManager().addNewEntries(request, entries);
+                if ( !okToContinue(actionId,
+                                   "Processed " + entries.size()
+                                   + " entries")) {
+                    return;
+                }
+                for (Entry entry : entries) {
+                    entry.setUser(request.getUser());
+                }
+                getEntryManager().addNewEntries(request, entries);
+                getActionManager().setContinueHtml(
+                    actionId,
+                    entries.size() + " entries created" + HtmlUtils.br()
+                    + HtmlUtils.href(
+                        request.url(
+                            getRepository().URL_ENTRY_SHOW, ARG_ENTRYID,
+                            parentEntry.getId()), "Continue"));
 
-        return new Result(request.entryUrl(getRepository().URL_ENTRY_SHOW, parentEntry));
+            }
+        };
+
+        return getActionManager().doAction(request, action,
+                                           "Importing the services", "",
+                                           parentEntry);
+
 
     }
 
 
-    private  JSONTokener getTokenizer(String url) throws Exception {
-        url = url +"?f=pjson";
-        String json = IOUtil.readContents(url.toString(), getClass());
+    /**
+     * _more_
+     *
+     * @param url _more_
+     *
+     * @return _more_
+     *
+     * @throws Exception _more_
+     */
+    private JSONTokener getTokenizer(String url) throws Exception {
+        url = url + "?f=pjson";
+        String      json = IOUtil.readContents(url.toString(), getClass());
         JSONTokener tokenizer = new JSONTokener(json);
+
         return tokenizer;
     }
 
@@ -138,32 +187,50 @@ public class EsriServiceImporter extends ImportHandler {
      * _more_
      *
      *
+     *
      * @param request _more_
      * @param parentEntry _more_
+     * @param actionId _more_
      * @param entries _more_
-     * @param esriServiceUrl _more_
-     *
+     * @param baseUrl _more_
+     * @param serviceUrl _more_
      * @throws Exception _more_
      */
     private void processServiceList(Request request, Entry parentEntry,
-                                    List<Entry> entries,
-                                    String baseUrl,
-                                    String serviceUrl)
+                                    Object actionId, List<Entry> entries,
+                                    String baseUrl, String serviceUrl)
             throws Exception {
-        
-        System.err.println("EsriServiceImporter: url: " + serviceUrl);
-        JSONObject  obj       = new JSONObject(getTokenizer(serviceUrl));
 
-        if(obj.has(TAG_FOLDERS)) {
-            JSONArray   folders  = obj.getJSONArray(TAG_FOLDERS);
+        if ( !okToContinue(actionId,
+                           "Processed " + entries.size() + " entries")) {
+            return;
+        }
+
+        System.err.println("EsriServiceImporter: url: " + serviceUrl);
+        JSONObject obj = new JSONObject(getTokenizer(serviceUrl));
+
+        if ( !okToContinue(actionId,
+                           "Processed " + entries.size() + " entries")) {
+            return;
+        }
+
+        if (obj.has(TAG_FOLDERS)) {
+            JSONArray folders = obj.getJSONArray(TAG_FOLDERS);
             for (int i = 0; i < folders.length(); i++) {
+                if ( !okToContinue(actionId,
+                                   "Processed " + entries.size()
+                                   + " entries")) {
+                    return;
+                }
+
                 String folder = folders.getString(i);
-                System.err.println("EsriServiceImporter: making folder:" + folder);
+                System.err.println("EsriServiceImporter: making folder:"
+                                   + folder);
                 Entry folderEntry = getRepository().getTypeHandler(
-                                                                   "type_esri_restfolder").createEntry(
-                                                                                                       getRepository().getGUID());
-                Date now = new Date();
-                String url = baseUrl +"/" + folder;
+                                        "type_esri_restfolder").createEntry(
+                                        getRepository().getGUID());
+                Date   now = new Date();
+                String url = baseUrl + "/" + folder;
                 folderEntry.setResource(new Resource(url, Resource.TYPE_URL));
                 folderEntry.setCreateDate(now.getTime());
                 folderEntry.setChangeDate(now.getTime());
@@ -173,25 +240,58 @@ public class EsriServiceImporter extends ImportHandler {
                 folderEntry.setName(getNameFromId(folder));
                 folderEntry.setParentEntryId(parentEntry.getId());
                 entries.add(folderEntry);
-                processServiceList(request, folderEntry,
-                                   entries,
+                processServiceList(request, folderEntry, actionId, entries,
                                    baseUrl, url);
             }
         }
 
 
-        if(obj.has(TAG_SERVICES)) {
-            JSONArray   services  = obj.getJSONArray(TAG_SERVICES);
+        if (obj.has(TAG_SERVICES)) {
+            JSONArray services = obj.getJSONArray(TAG_SERVICES);
             for (int i = 0; i < services.length(); i++) {
                 JSONObject service = services.getJSONObject(i);
-                processService(request, parentEntry, entries, baseUrl, service);
+                if ( !okToContinue(actionId,
+                                   "Processed " + entries.size()
+                                   + " entries")) {
+                    return;
+                }
+                processService(request, parentEntry, actionId, entries,
+                               baseUrl, service);
             }
         }
     }
 
+    /**
+     * _more_
+     *
+     * @param actionId _more_
+     * @param message _more_
+     *
+     * @return _more_
+     */
+    private boolean okToContinue(Object actionId, String message) {
+        if ( !getRepository().getActionManager().getActionOk(actionId)) {
+            return false;
+        }
+        if (message != null) {
+            getActionManager().setActionMessage(actionId, message);
+        }
+
+        return true;
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param id _more_
+     *
+     * @return _more_
+     */
     private String getNameFromId(String id) {
-        String[]toks = id.split("/");
-        return toks[toks.length-1];
+        String[] toks = id.split("/");
+
+        return toks[toks.length - 1];
     }
 
 
@@ -201,43 +301,45 @@ public class EsriServiceImporter extends ImportHandler {
      *
      * @param request _more_
      * @param parentEntry _more_
+     * @param actionId _more_
      * @param entries _more_
+     * @param baseUrl _more_
      * @param service _more_
      *
      * @throws Exception _more_
      */
     private void processService(Request request, Entry parentEntry,
-                                List<Entry> entries, 
-                                String baseUrl, 
-                                JSONObject service)
+                                Object actionId, List<Entry> entries,
+                                String baseUrl, JSONObject service)
             throws Exception {
-        String      id        = service.getString("name");
-        String      type      = service.getString("type");
-        String      url = null;
+        String id   = service.getString("name");
+        String type = service.getString("type");
+        String url  = null;
 
         //        http://services.nationalmap.gov/arcgis/rest/services?f=pjson
-        if(service.has(TAG_URL)) {
+        if (service.has(TAG_URL)) {
             url = service.getString(TAG_URL);
         } else {
-            url = baseUrl +"/" + id +"/" + type;
+            url = baseUrl + "/" + id + "/" + type;
         }
-        System.err.println ("EsriServiceImporter.processService:"  + url);
+        System.err.println("EsriServiceImporter.processService:" + url);
 
-        JSONObject  obj       = new JSONObject(getTokenizer(url));
-        String      name      = null;
-        if(obj.has(TAG_SERVICEDESCRIPTION)) {
+        JSONObject obj  = new JSONObject(getTokenizer(url));
+        String     name = null;
+        if (obj.has(TAG_SERVICEDESCRIPTION)) {
             obj.getString(TAG_SERVICEDESCRIPTION);
         }
         if ( !Utils.stringDefined(name)) {
             name = getNameFromId(id);
         }
         String description = "";
-        if(obj.has(TAG_DESCRIPTION)) 
-            description  = obj.getString(TAG_DESCRIPTION);
-        else         if(obj.has(TAG_SERVICEDESCRIPTION)) 
-            description  = obj.getString(TAG_SERVICEDESCRIPTION);
+        if (obj.has(TAG_DESCRIPTION)) {
+            description = obj.getString(TAG_DESCRIPTION);
+        } else if (obj.has(TAG_SERVICEDESCRIPTION)) {
+            description = obj.getString(TAG_SERVICEDESCRIPTION);
+        }
 
-        String wkid        = null;
+        String wkid = null;
 
 
         if (obj.has(TAG_FULLEXTENT)) {
@@ -256,14 +358,21 @@ public class EsriServiceImporter extends ImportHandler {
         }
 
 
-        Entry entry = getRepository().getTypeHandler(
-                          "type_esri_restservice").createEntry(
+        String entryType = "type_esri_restservice";
+        if (type.equals("FeatureServer")) {
+            entryType = "type_esri_featureserver";
+        } else if (type.equals("MapServer")) {
+            entryType = "type_esri_mapserver";
+        } else if (type.equals("ImageServer")) {
+            entryType = "type_esri_imageserver";
+        }
+
+        Entry entry = getRepository().getTypeHandler(entryType).createEntry(
                           getRepository().getGUID());
         Object[] values = entry.getTypeHandler().getEntryValues(entry);
 
 
         values[0] = id;
-        values[1] = type;
 
 
         Date now = new Date();
