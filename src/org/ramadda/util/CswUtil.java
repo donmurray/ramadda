1a8fec4eca47647d22994178a3574033e42113b9
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-09-03 11:33:35 +0000

    Add some tags

--- src/org/ramadda/util/CswUtil.java
+++ src/org/ramadda/util/CswUtil.java
@@ -1,5 +1,5 @@
 /*
-* Copyright 2008-2013 Geode Systems LLC
+* Copyright 2008-2014 Geode Systems LLC
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
 * software and associated documentation files (the "Software"), to deal in the Software 
@@ -36,9 +36,6 @@ import java.util.Date;
 public class CswUtil {
 
 
-
-
-
     /*
 
     endpoint = 'http://www.ngdc.noaa.gov/geoportal/csw' # NGDC Geoportal
@@ -54,6 +51,91 @@ public class CswUtil {
     #endpoint = 'https://edg.epa.gov/metadata/csw'  # EPA
     #endpoint = 'http://cwic.csiss.gmu.edu/cwicv1/discovery' */
 
+
+
+
+    /** _more_          */
+    public static final String TAG_CSW_RECORD = "csw:Record";
+
+    /** _more_          */
+    public static final String TAG_DCT_ABSTRACT = "dct:abstract";
+
+    /** _more_          */
+    public static final String TAG_DCT_MODIFIED = "dct:modified";
+
+    /** _more_          */
+    public static final String TAG_DC_CONTRIBUTOR = "dc:contributor";
+
+    /** _more_          */
+    public static final String TAG_DC_CREATOR = "dc:creator";
+
+    /** _more_          */
+    public static final String TAG_DC_FORMAT = "dc:format";
+
+    /** _more_          */
+    public static final String TAG_DC_IDENTIFIER = "dc:identifier";
+
+    /** _more_          */
+    public static final String TAG_DC_LANGUAGE = "dc:language";
+
+    /** _more_          */
+    public static final String TAG_DC_PUBLISHER = "dc:publisher";
+
+    /** _more_          */
+    public static final String TAG_DC_RELATION = "dc:relation";
+
+    /** _more_          */
+    public static final String TAG_DC_RIGHTS = "dc:rights";
+
+    /** _more_          */
+    public static final String TAG_DC_SOURCE = "dc:source";
+
+    /** _more_          */
+    public static final String TAG_DC_SUBJECT = "dc:subject";
+
+    /** _more_          */
+    public static final String TAG_DC_TITLE = "dc:title";
+
+    /** _more_          */
+    public static final String TAG_DC_TYPE = "dc:type";
+
+    /** _more_          */
+    public static final String TAG_OWS_BOUNDINGBOX = "ows:BoundingBox";
+
+    /** _more_          */
+    public static final String TAG_OWS_LOWERCORNER = "ows:LowerCorner";
+
+    /** _more_          */
+    public static final String TAG_OWS_UPPERCORNER = "ows:UpperCorner";
+
+    /** _more_          */
+    public static final String TAG_OWS_WGS84BOUNDINGBOX =
+        "ows:WGS84BoundingBox";
+
+    /** _more_          */
+    public static final String TAG_RECORD = "Record";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS =
+        "http://www.opengis.net/cat/csw/2.0.2";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_CSW =
+        "http://www.opengis.net/cat/csw/2.0.2";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_DC =
+        "http://purl.org/dc/elements/1.1/";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_DCT = "http://purl.org/dc/terms/";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_OWS = "http://www.opengis.net/ows";
+
+    /** _more_          */
+    public static final String XMLNS_XMLNS_XSI =
+        "http://www.w3.org/2001/XMLSchema-instance";
 
 
 }
