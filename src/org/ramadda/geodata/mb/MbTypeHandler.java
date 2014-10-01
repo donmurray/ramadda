86a0b166108a23dce86c8e631bc6268bcfd55c17
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-10-01 04:54:16 +0000

    new package for mbsystem bathymetry data

---
+++ src/org/ramadda/geodata/mb/MbTypeHandler.java
@@ -0,0 +1,178 @@
+/*
+* Copyright 2008-2014 Geode Systems LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.geodata.mb;
+
+
+
+import org.ramadda.data.process.Service;
+import org.ramadda.data.process.ServiceOutput;
+import org.ramadda.repository.*;
+import org.ramadda.repository.metadata.Metadata;
+import org.ramadda.repository.type.*;
+import org.ramadda.util.Utils;
+
+
+
+import org.w3c.dom.*;
+
+import ucar.nc2.units.DateUnit;
+
+import ucar.unidata.util.IOUtil;
+import ucar.unidata.util.Misc;
+import ucar.unidata.util.StringUtil;
+import ucar.unidata.xml.XmlUtil;
+
+import java.io.File;
+
+
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ *
+ *
+ * @author Jeff McWhirter
+ */
+public class MbTypeHandler extends GenericTypeHandler {
+
+    /**
+     * _more_
+     *
+     * @param repository _more_
+     * @param node _more_
+     * @throws Exception On badness
+     */
+    public MbTypeHandler(Repository repository, Element node)
+            throws Exception {
+        super(repository, node);
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param entry _more_
+     *
+     * @throws Exception On badness
+     */
+    @Override
+    public void initializeNewEntry(Entry entry) throws Exception {
+        Object[] values = getEntryValues(entry);
+        String suffix =
+            IOUtil.getFileExtension(entry.getResource().getPath());
+        suffix    = suffix.replace(".mb", "");
+        values[0] = suffix;
+        super.initializeNewEntry(entry);
+    }
+
+
+
+    /**
+     * _more_
+     *
+     * @param entry _more_
+     * @param service _more_
+     * @param output _more_
+     *
+     * @throws Exception _more_
+     */
+    @Override
+    public void handleServiceResults(Entry entry, Service service,
+                                     ServiceOutput output)
+            throws Exception {
+        List<Entry> entries = output.getEntries();
+        if (entries.size() == 0) {
+            return;
+        }
+        String xml = IOUtil.readContents(entries.get(0).getFile().toString(),
+                                         getClass(), "");
+        Element root = null;
+
+        System.err.println(xml);
+
+        try {
+            root = XmlUtil.getRoot(xml);
+        } catch (Exception exc) {
+            xml  = Utils.removeNonAscii(xml);
+            root = XmlUtil.getRoot(xml);
+        }
+        Element limits   = XmlUtil.findChild(root, MbUtil.TAG_LIMITS);
+
+        Element fileInfo = XmlUtil.findChild(root, MbUtil.TAG_FILE_INFO);
+        entry.setDescription(XmlUtil.getGrandChildText(fileInfo,
+                MbUtil.TAG_INFORMAL_DESCRIPTION));
+
+        for (String attr :
+                StringUtil.split(XmlUtil.getGrandChildText(fileInfo,
+                    MbUtil.TAG_ATTRIBUTES), ",", true, true)) {
+            entry.addMetadata(new Metadata(getRepository().getGUID(),
+                                           entry.getId(), "enum_tag", false,
+                                           attr, "", "", "", ""));
+
+        }
+
+        entry.setNorth(Double.parseDouble(XmlUtil.getGrandChildText(limits,
+                MbUtil.TAG_MAXIMUM_LATITUDE)));
+        entry.setSouth(Double.parseDouble(XmlUtil.getGrandChildText(limits,
+                MbUtil.TAG_MINIMUM_LATITUDE)));
+        entry.setEast(Double.parseDouble(XmlUtil.getGrandChildText(limits,
+                MbUtil.TAG_MAXIMUM_LONGITUDE)));
+        entry.setWest(Double.parseDouble(XmlUtil.getGrandChildText(limits,
+                MbUtil.TAG_MINIMUM_LONGITUDE)));
+
+
+        Element startOfData = XmlUtil.findChild(root,
+                                  MbUtil.TAG_START_OF_DATA);
+        Element endOfData = XmlUtil.findChild(root, MbUtil.TAG_END_OF_DATA);
+
+
+        Date startDate =
+            DateUnit.getStandardOrISO(XmlUtil.getGrandChildText(startOfData,
+                MbUtil.TAG_TIME_ISO));
+        Date endDate =
+            DateUnit.getStandardOrISO(XmlUtil.getGrandChildText(endOfData,
+                MbUtil.TAG_TIME_ISO));
+        entry.setStartDate(startDate.getTime());
+        entry.setEndDate(endDate.getTime());
+
+
+        int numberOfRecords =
+            Integer.parseInt(
+                XmlUtil.getGrandChildText(
+                    XmlUtil.findChild(root, MbUtil.TAG_DATA_TOTALS),
+                    MbUtil.TAG_NUMBER_OF_RECORDS));
+
+        Object[] values = getEntryValues(entry);
+        values[1] = new Integer(numberOfRecords);
+
+        Element navTotals = XmlUtil.findChild(root,
+                                MbUtil.TAG_TNAVIGATION_TOTALS);
+        values[2] = new Double(XmlUtil.getGrandChildText(navTotals,
+                MbUtil.TAG_TOTAL_TRACK_LENGTH_KM));
+
+    }
+
+
+
+
+
+}
---
+++ src/org/ramadda/geodata/mb/MbUtil.java
@@ -0,0 +1,228 @@
+/*
+* Copyright 2008-2014 Geode Systems LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the "Software"), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, 
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+* permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies 
+* or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*/
+
+package org.ramadda.geodata.mb;
+
+
+/**
+ * Class description
+ *
+ *
+ * @version        $version$, Tue, Sep 30, '14
+ * @author         Enter your name here...
+ */
+public class MbUtil {
+
+    /** _more_ */
+    public static final String TAG_MBINFO = "mbinfo";
+
+    /** _more_ */
+    public static final String TAG_FILE_INFO = "file_info";
+
+    /** _more_ */
+    public static final String TAG_SWATH_DATA_FILE = "swath_data_file";
+
+    /** _more_ */
+    public static final String TAG_MBIO_DATA_FORMAT_ID =
+        "mbio_data_format_id";
+
+    /** _more_ */
+    public static final String TAG_FORMAT_NAME = "format_name";
+
+    /** _more_ */
+    public static final String TAG_INFORMAL_DESCRIPTION =
+        "informal_description";
+
+    /** _more_ */
+    public static final String TAG_ATTRIBUTES = "attributes";
+
+    /** _more_ */
+    public static final String TAG_DATA_TOTALS = "data_totals";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_OF_RECORDS = "number_of_records";
+
+    /** _more_ */
+    public static final String TAG_BATHYMETRY_DATA = "bathymetry_data";
+
+    /** _more_ */
+    public static final String TAG_MAX_BEAMS_PER_PING = "max_beams_per_ping";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_BEAMS = "number_beams";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_GOOD_BEAMS = "number_good_beams";
+
+    /** _more_ */
+    public static final String TAG_PERCENT_GOOD_BEAMS = "percent_good_beams";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_ZERO_BEAMS = "number_zero_beams";
+
+    /** _more_ */
+    public static final String TAG_PERCENT_ZERO_BEAMS = "percent_zero_beams";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_FLAGGED_BEAMS =
+        "number_flagged_beams";
+
+    /** _more_ */
+    public static final String TAG_PERCENT_FLAGGED_BEAMS =
+        "percent_flagged_beams";
+
+    /** _more_ */
+    public static final String TAG_AMPLITUDE_DATA = "amplitude_data";
+
+    /** _more_ */
+    public static final String TAG_SIDESCAN_DATA = "sidescan_data";
+
+    /** _more_ */
+    public static final String TAG_MAX_PIXELS_PER_PING =
+        "max_pixels_per_ping";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_PIXELS = "number_pixels";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_GOOD_PIXELS = "number_good_pixels";
+
+    /** _more_ */
+    public static final String TAG_PERCENT_GOOD_PIXELS =
+        "percent_good_pixels";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_ZERO_PIXELS = "number_zero_pixels";
+
+    /** _more_ */
+    public static final String TAG_PERCENT_ZERO_PIXELS =
+        "percent_zero_pixels";
+
+    /** _more_ */
+    public static final String TAG_NUMBER_FLAGGED_PIXELS =
+        "number_flagged_pixels";
+
+    /** _more_ */
+    public static final String TAG_PERCENT_FLAGGED_PIXELS =
+        "percent_flagged_pixels";
+
+    /** _more_ */
+    public static final String TAG_TNAVIGATION_TOTALS = "tnavigation_totals";
+
+    /** _more_ */
+    public static final String TAG_TOTAL_TIME_HOURS = "total_time_hours";
+
+    /** _more_ */
+    public static final String TAG_TOTAL_TRACK_LENGTH_KM =
+        "total_track_length_km";
+
+    /** _more_ */
+    public static final String TAG_AVERAGE_SPEED_KM_PER_HR =
+        "average_speed_km_per_hr";
+
+    /** _more_ */
+    public static final String TAG_AVERAGE_SPEED_KNOTS =
+        "average_speed_knots";
+
+    /** _more_ */
+    public static final String TAG_START_OF_DATA = "start_of_data";
+
+    /** _more_ */
+    public static final String TAG_TIME = "time";
+
+    /** _more_ */
+    public static final String TAG_TIME_ISO = "time_iso";
+
+    /** _more_ */
+    public static final String TAG_LONGITUDE = "longitude";
+
+    /** _more_ */
+    public static final String TAG_LATITUDE = "latitude";
+
+    /** _more_ */
+    public static final String TAG_DEPTH_METERS = "depth_meters";
+
+    /** _more_ */
+    public static final String TAG_SPEED_KM_PER_HOUR = "speed_km_per_hour";
+
+    /** _more_ */
+    public static final String TAG_SPEED_KNOTS = "speed_knots";
+
+    /** _more_ */
+    public static final String TAG_HEADING_DEGREES = "heading_degrees";
+
+    /** _more_ */
+    public static final String TAG_SONAR_DEPTH_METERS = "sonar_depth_meters";
+
+    /** _more_ */
+    public static final String TAG_SONAR_ALTITUDE_METERS =
+        "sonar_altitude_meters";
+
+    /** _more_ */
+    public static final String TAG_END_OF_DATA = "end_of_data";
+
+    /** _more_ */
+    public static final String TAG_LIMITS = "limits";
+
+    /** _more_ */
+    public static final String TAG_MINIMUM_LONGITUDE = "minimum_longitude";
+
+    /** _more_ */
+    public static final String TAG_MAXIMUM_LONGITUDE = "maximum_longitude";
+
+    /** _more_ */
+    public static final String TAG_MINIMUM_LATITUDE = "minimum_latitude";
+
+    /** _more_ */
+    public static final String TAG_MAXIMUM_LATITUDE = "maximum_latitude";
+
+    /** _more_ */
+    public static final String TAG_MINIMUM_SONAR_DEPTH =
+        "minimum_sonar_depth";
+
+    /** _more_ */
+    public static final String TAG_MAXIMUM_SONAR_DEPTH =
+        "maximum_sonar_depth";
+
+    /** _more_ */
+    public static final String TAG_MINIMUM_ALTITUDE = "minimum_altitude";
+
+    /** _more_ */
+    public static final String TAG_MAXIMUM_ALTITUDE = "maximum_altitude";
+
+    /** _more_ */
+    public static final String TAG_MINIMUM_DEPTH = "minimum_depth";
+
+    /** _more_ */
+    public static final String TAG_MAXIMUM_DEPTH = "maximum_depth";
+
+    /** _more_ */
+    public static final String TAG_MINIMUM_AMPLITUDE = "minimum_amplitude";
+
+    /** _more_ */
+    public static final String TAG_MAXIMUM_AMPLITUDE = "maximum_amplitude";
+
+    /** _more_ */
+    public static final String TAG_MAXIMUM_SIDESCAN = "maximum_sidescan";
+
+
+
+
+}
