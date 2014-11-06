7b3c4437382b8dca56f8b7b80e09d8ddb1ef28ec
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-11-06 21:10:44 +0000

    harvest more metadata. Define some metadata types

--- src/org/ramadda/bio/genomics/PdbTypeHandler.java
+++ src/org/ramadda/bio/genomics/PdbTypeHandler.java
@@ -25,7 +25,7 @@ import org.ramadda.repository.*;
 import org.ramadda.repository.metadata.*;
 import org.ramadda.repository.type.*;
 import org.ramadda.util.Utils;
-    
+
 
 import org.w3c.dom.*;
 
@@ -38,8 +38,9 @@ import ucar.unidata.xml.XmlUtil;
 
 import java.io.*;
 
-import java.util.Date;
 import java.util.ArrayList;
+
+import java.util.Date;
 import java.util.List;
 
 import javax.mail.*;
@@ -71,12 +72,16 @@ public class PdbTypeHandler extends GenericTypeHandler {
     /**
      * _more_
      *
+     *
+     * @param request _more_
      * @param entry _more_
      *
      * @throws Exception _more_
      */
     @Override
-    public void initializeNewEntry(Request request, Entry entry) throws Exception {
+    public void initializeNewEntry(Request request, Entry entry)
+            throws Exception {
+
         super.initializeNewEntry(request, entry);
 
         //If the file for the entry does not exist then return
@@ -84,48 +89,114 @@ public class PdbTypeHandler extends GenericTypeHandler {
             return;
         }
 
-        List<String> titles = new ArrayList<String>();
-        List<String> remarks = new ArrayList<String>();
-        List<String> keywords = new ArrayList<String>();
-        InputStream is= Utils.doMakeInputStream(entry.getResource().getPath(),true);
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        int cnt =0;
-        while(true) {
+        List<String> titles    = new ArrayList<String>();
+        List<String> remarks   = new ArrayList<String>();
+        List<String> keywords  = new ArrayList<String>();
+        List<String> compounds = new ArrayList<String>();
+        List<String> sources   = new ArrayList<String>();
+        List<String> authors   = new ArrayList<String>();
+        InputStream is =
+            Utils.doMakeInputStream(entry.getResource().getPath(), true);
+        BufferedReader br  = new BufferedReader(new InputStreamReader(is));
+        int            cnt = 0;
+        while (true) {
             cnt++;
             String line = br.readLine();
-            if(line == null) break;
-            if(line.startsWith("TITLE ")) {
+            if (line == null) {
+                break;
+            }
+            if (line.startsWith("TITLE ")) {
                 //                System.err.println (line);
-                titles.add(line.substring("TITLE ".length()));
-            } else if(line.startsWith("REMARK ")) {
+                titles.add(
+                    line.substring("TITLE ".length()).replaceAll(
+                        "^\\s*[0-9]+", ""));
+            } else if (line.startsWith("REMARK ")) {
                 //                System.err.println (line);
-                String remark  = line.substring("REMARK ".length()).trim();
-                int idx = remark.indexOf(" ");
-                if(idx>0) {
-                    remark = remark.substring(idx+1);
+                String remark = line.substring("REMARK ".length()).trim();
+                int    idx    = remark.indexOf(" ");
+                if (idx > 0) {
+                    remark = remark.substring(idx + 1);
+                } else {
+                    continue;
                 }
                 remarks.add(remark);
-            } else if(line.startsWith("KEYWDS ")) {
-                keywords.addAll(StringUtil.split(line.substring("KEYWDS ".length()).trim(), ",", true, true));
+            } else if (line.startsWith("EXPDATA ")) {
+                entry.addMetadata(new Metadata(getRepository().getGUID(),
+                        entry.getId(), "bio_method", true,
+                        line.substring("EXPDATA ".length()), "", "", "", ""));
+
+            } else if (line.startsWith("KEYWDS ")) {
+                keywords.addAll(
+                    StringUtil.split(
+                        line.substring("KEYWDS ".length()).trim(), ",", true,
+                        true));
+            } else if (line.startsWith("COMPND ")) {
+                compounds.add(line.substring("COMPND ".length()).trim());
+            } else if (line.startsWith("SOURCE ")) {
+                sources.add(line.substring("SOURCE ".length()).trim());
+            } else if (line.startsWith("AUTHOR ")) {
+                authors.addAll(
+                    StringUtil.split(
+                        line.substring("AUTHOR ".length()).trim(), ",", true,
+                        true));
             }
 
         }
 
-        for(String word: keywords) {
+        for (String compound : compounds) {
+            compound = compound.replaceAll("^\\s*[0-9]+", "");
+            compound = compound.replaceAll(";$", "");
+            List<String> toks = StringUtil.splitUpTo(compound, ":", 2);
             entry.addMetadata(new Metadata(getRepository().getGUID(),
-                                           entry.getId(), "content.keyword", true, word, "", "",
-                                           "", ""));
+                                           entry.getId(), "bio_pdb_compound",
+                                           true, toks.get(0),
+                                           ((toks.size() <= 1)
+                                            ? ""
+                                            : toks.get(1)), "", "", ""));
+
+
         }
-        if(titles.size()>0) {
+
+        for (String source : sources) {
+            source = source.replaceAll("^\\s*[0-9]+", "");
+            source = source.replaceAll(";$", "");
+            List<String> toks = StringUtil.splitUpTo(source, ":", 2);
+            entry.addMetadata(new Metadata(getRepository().getGUID(),
+                                           entry.getId(), "bio_pdb_source",
+                                           true, toks.get(0),
+                                           ((toks.size() <= 1)
+                                            ? ""
+                                            : toks.get(1)), "", "", ""));
+
+
+        }
+
+        for (String word : authors) {
+            entry.addMetadata(new Metadata(getRepository().getGUID(),
+                                           entry.getId(), "bio_author", true,
+                                           word, "", "", "", ""));
+        }
+
+
+        for (String word : keywords) {
+            entry.addMetadata(new Metadata(getRepository().getGUID(),
+                                           entry.getId(), "content.keyword",
+                                           true, word, "", "", "", ""));
+        }
+        if (titles.size() > 0) {
             entry.setName(StringUtil.join(" ", titles));
         }
-        if(remarks.size()>0) {
-            entry.setDescription("<pre><div style=\"max-height: 300px; overflow-y:auto;\">" + StringUtil.join("\n", remarks)+"</div></pre>");
+        if (remarks.size() > 0) {
+
+            entry.setDescription(
+                "<pre><div style=\"max-height: 300px; overflow-y:auto;\">"
+                + StringUtil.join("\n", remarks) + "</div></pre>");
         }
         /*
           entry.getFile().toString();
           Object[] values = getEntryValues(entry);
         */
+
 
     }
 
