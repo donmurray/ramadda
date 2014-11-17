637c889e54374dcff7ed7295c7f16b03d817d9cb
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2014-11-17 13:50:10 +0000

    snapshot

--- src/org/ramadda/plugins/media/DbTableTypeHandler.java
+++ src/org/ramadda/plugins/media/DbTableTypeHandler.java
@@ -24,14 +24,13 @@ package org.ramadda.plugins.media;
 import org.ramadda.repository.*;
 import org.ramadda.repository.metadata.*;
 import org.ramadda.repository.type.*;
-import org.ramadda.util.HtmlUtils;
 
 import org.ramadda.sql.*;
+import org.ramadda.util.HtmlUtils;
 
 import org.ramadda.util.Json;
 import org.ramadda.util.Utils;
 import org.ramadda.util.XlsUtil;
-import java.sql.*;
 
 
 import org.w3c.dom.*;
@@ -48,11 +47,13 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import java.sql.*;
+
 import java.util.ArrayList;
 
 import java.util.Date;
-import java.util.Hashtable;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.List;
 
 import java.util.zip.ZipEntry;
@@ -65,12 +66,22 @@ import java.util.zip.ZipInputStream;
  */
 public class DbTableTypeHandler extends TabularTypeHandler {
 
+    /** _more_          */
     private static int IDX = TabularTypeHandler.IDX_LAST;
 
+    /** _more_          */
     public static final int IDX_JDBC = IDX++;
+
+    /** _more_          */
     public static final int IDX_USER = IDX++;
+
+    /** _more_          */
     public static final int IDX_PASSWORD = IDX++;
+
+    /** _more_          */
     public static final int IDX_TABLE = IDX++;
+
+    /** _more_          */
     public static final int IDX_COLUMNS = IDX++;
 
 
@@ -83,75 +94,105 @@ public class DbTableTypeHandler extends TabularTypeHandler {
      *
      * @throws Exception _more_
      */
-    public  DbTableTypeHandler(Repository repository, Element entryNode)
+    public DbTableTypeHandler(Repository repository, Element entryNode)
             throws Exception {
         super(repository, entryNode);
     }
 
-
+    /**
+     * _more_
+     *
+     * @return _more_
+     */
     @Override
-        public void read(Request request, Entry entry, InputStream myxls, List<String> sheets, HashSet<Integer> sheetsToShow, int skip) throws Exception {
+    public boolean adminOnly() {
+        return true;
+    }
+
+
+
+    /**
+     * _more_
+     *
+     * @param request _more_
+     * @param entry _more_
+     * @param myxls _more_
+     * @param sheets _more_
+     * @param sheetsToShow _more_
+     * @param skip _more_
+     *
+     * @throws Exception _more_
+     */
+    @Override
+    public void read(Request request, Entry entry, InputStream myxls,
+                     List<String> sheets, HashSet<Integer> sheetsToShow,
+                     int skip)
+            throws Exception {
 
         //        Object[]values = entry.getValues();
         //        for(int i=0;i<values.length;i++) {
-            //            System.err.println (i +" =  "  + values[i]);
+        //            System.err.println (i +" =  "  + values[i]);
         //        }
 
-        String table = entry.getValue(IDX_TABLE,(String)null);
-        System.err.println ("table:" + table +" idx:" + IDX_TABLE);
+        String table = entry.getValue(IDX_TABLE, (String) null);
+        System.err.println("table:" + table + " idx:" + IDX_TABLE);
 
 
-        if(!Utils.stringDefined(table)) {
+        if ( !Utils.stringDefined(table)) {
             System.err.println("db-read- no table");
+
             return;
         }
 
-        List<String> cols = StringUtil.split(entry.getValue(IDX_COLUMNS,""),"\n",true,true);
+        List<String> cols = StringUtil.split(entry.getValue(IDX_COLUMNS, ""),
+                                             "\n", true, true);
         String what = "*";
 
-        if(cols.size()>0) {
-            what = StringUtil.join(",",cols);
+        if (cols.size() > 0) {
+            what = StringUtil.join(",", cols);
         }
 
-        int max  = TabularOutputHandler.MAX_ROWS;
+        int max = TabularOutputHandler.MAX_ROWS;
         Statement stmt = getDatabaseManager().select(what,
-                             Misc.newList(table),
-                                                     null, "", max);
+                             Misc.newList(table), null, "", max);
 
-        SqlUtil.Iterator iter = getDatabaseManager().getIterator(stmt);
-        ResultSet        results;
-
+        SqlUtil.Iterator  iter = getDatabaseManager().getIterator(stmt);
+        ResultSet         results;
 
 
 
-        List<String> rows = new ArrayList<String>();
+
+        List<String>      rows = new ArrayList<String>();
         ResultSetMetaData rsmd = null;
 
         while ((results = iter.getNext()) != null) {
-            if(rsmd == null) {
+            if (rsmd == null) {
                 rsmd = results.getMetaData();
-                int columnCount = rsmd.getColumnCount();
-                List<String> names = new ArrayList<String>();
-                for (int i = 1; i < columnCount + 1; i++ ) {
+                int          columnCount = rsmd.getColumnCount();
+                List<String> names       = new ArrayList<String>();
+                for (int i = 1; i < columnCount + 1; i++) {
                     String name = rsmd.getColumnName(i);
                     names.add(Json.quote(name));
-                }                
+                }
                 rows.add(Json.list(names));
             }
             List<String> row = new ArrayList<String>();
-            for(int col=0;col< rsmd.getColumnCount();col++) {
-                String s = results.getString(col+1);
-                if(s == null) s = "null";
-                else s = Json.quote(s);
+            for (int col = 0; col < rsmd.getColumnCount(); col++) {
+                String s = results.getString(col + 1);
+                if (s == null) {
+                    s = "null";
+                } else {
+                    s = Json.quote(s);
+                }
                 row.add(s);
             }
             rows.add(Json.list(row));
         }
 
-        sheets.add(Json.map("name", Json.quote(table),
-                            "rows", Json.list(rows)));
+        sheets.add(Json.map("name", Json.quote(table), "rows",
+                            Json.list(rows)));
 
-        
+
 
     }
 }
