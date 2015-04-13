442c8052a48fd26a1971958157346919903f86d1
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2015-04-13 19:22:56 +0000

    snapshot

--- stuff/scicloud/misc/installall.sh
+++ stuff/scicloud/misc/installall.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
-
 dir=`dirname $0`
-
 bindir=/home/ec2-user/bin
 mkdir -p ${bindir}
-
 sudo yum-config-manager --enable epel
 
-yum install ftp -y
-yum install wget -y
-yum install gcc -y
-yum install gcc-c++ -y
-yum install cmake -y
-yum install m4 -y
 
 
-yum install ImageMagick -y
