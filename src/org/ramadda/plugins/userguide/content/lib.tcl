c443d22bb8ca9ebf28d9f92431e81ee54b42bab7
jmcwhirter <jmcwhirter@e4cd2953-5001-4204-a071-608bb2a1fa94>
2015-05-27 12:59:00 +0000

    docs updates

--- src/org/ramadda/plugins/userguide/content/lib.tcl
+++ src/org/ramadda/plugins/userguide/content/lib.tcl
@@ -5,7 +5,7 @@
 
 gen::setDoTclEvaluation 1
 gen::setTargetDir ../htdocs/userguide
-gen::defineMacro {<%ramadda.version%>} {2.0}
+gen::defineMacro {<%ramadda.version%>} {2.1b}
 
 
 namespace eval wiki {}
